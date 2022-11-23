ebe5d773f6a5d8aafa0e277111aa131ece5d37c2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-23 15:58:44 +0200

    Change name

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const feedbackController = require('../controllers/feedbackController');
 
-router.use('/feedback', feedbackController);
+router.use('/feedbacks', feedbackController);
 
 module.exports = router;
