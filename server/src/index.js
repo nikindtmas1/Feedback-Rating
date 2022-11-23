9924f333ab15ae51952cfbf54233b5f0ec27725c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-23 14:55:33 +0200

    Require routes

--- server/src/index.js
+++ server/src/index.js
@@ -7,8 +7,10 @@ const { development } = require("./config/config");
 const port = development.PORT;
 
 const mongooseConfig = require("./config/configMongoose");
+const routes = require('./routes/routes');
 
 app.use(cors());
+app.use(routes);
 mongooseConfig(app);
 
 app.listen(port, () => {
