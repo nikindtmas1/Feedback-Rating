6c550d31367386cb22ed9f19701df1d6f7ae4f30
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-23 10:38:18 +0200

    Formated the page

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
+  development: {
+    PORT: 5000,
+    dbAtlas:
+      "mongodb+srv://niki:niki548444@cluster0.8me3yuw.mongodb.net/?retryWrites=true&w=majority",
+    secretStr: "mnogoqkaparola",
+  },
 
-    development: {
-        PORT: 5000,
-        dbAtlas: 'mongodb+srv://niki:niki548444@cluster0.8me3yuw.mongodb.net/?retryWrites=true&w=majority',
-        secretStr: 'mnogoqkaparola'
-    },
-
-    production: {
-        PORT: 80
-    }
-}
+  production: {
+    PORT: 80,
+  },
+};
