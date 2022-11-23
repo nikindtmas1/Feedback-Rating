2822cb57fdd3abd4b5445e162c5909338a6d04f0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-23 15:59:22 +0200

    Added path

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     PORT: 5000,
     dbAtlas:
-      "mongodb+srv://niki:niki548444@cluster0.8me3yuw.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://niki:niki548444@cluster0.8me3yuw.mongodb.net/feedback_datas?retryWrites=true&w=majority",
     secretStr: "mnogoqkaparola",
   },
 
