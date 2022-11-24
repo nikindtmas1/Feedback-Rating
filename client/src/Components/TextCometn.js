c61edcf1a4867a523357a4a9996075b77bfc7039
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-24 13:21:44 +0200

    Create text component

---
+++ client/src/Components/TextCometn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextCometn = () => {
+  return (
+    <div className="text-container">
+      <div className="text-content">TextCometn</div>
+    </div>
+  );
+};
+
+export default TextCometn;
