5eb2050b2777b1b3549d92bb5f1d05d06bbfa8d4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-28 11:05:58 +0200

    Added function getAll

--- client/src/Components/services/data.js
+++ client/src/Components/services/data.js
@@ -1,6 +1,12 @@
-import * as service from './feedbackService';
+import * as service from "./feedbackService";
 
-const host = 'http://localhost:5000';
+const host = "http://localhost:5000";
 
 service.settings.host = host;
 
+export async function getAll() {
+  let feedbacks = await service.get(host + "/feedbacks");
+  let result = Object.values(feedbacks);
+
+  return result;
+}
