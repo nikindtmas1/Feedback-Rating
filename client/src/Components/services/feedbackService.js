a28108daa958010d74bec781e41d23f10e351a75
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-28 11:05:44 +0200

    Added function requeist

--- client/src/Components/services/feedbackService.js
+++ client/src/Components/services/feedbackService.js
@@ -1,3 +1,44 @@
 export const settings = {
-    host: "",
+  host: "",
 };
+
+async function request(url, options) {
+  try {
+    const respons = await fetch(url, options);
+
+    if (respons.ok === false) {
+      const error = await respons.json();
+
+      throw new Error(error.message);
+    }
+
+    try {
+      const data = await respons.json();
+
+      return data;
+    } catch (error) {
+      return respons;
+    }
+  } catch (error) {
+    alert(error.message);
+    throw error;
+  }
+}
+
+function getOptions(method = "get", body) {
+  const options = {
+    method,
+    headers: {},
+  };
+
+  if (body) {
+    options.headers["Content-Type"] = "application/json";
+    options.body = JSON.stringify(body);
+  }
+
+  return options;
+}
+
+export async function get(url) {
+  return await request(url, getOptions());
+}
