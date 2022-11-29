360d2f77d8df06cda01316c919914cd7d7289766
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-29 12:02:12 +0200

    Update CatalogFeedback

--- client/src/Components/CatalogFeedback.js
+++ client/src/Components/CatalogFeedback.js
@@ -2,14 +2,27 @@ import React, { useContext } from "react";
 import { motion, AnimatePresence } from 'framer-motion';
 import { FeedbackContext } from "../context/FeedbackContext";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from '../Components/shared/Spinner';
 
 const CatalogFeedback = () => {
-  let value = useContext(FeedbackContext);
-  console.log(value.feedback);
-  return (
+  const { feedback, isLoadding } = useContext(FeedbackContext);
+  // let value = useContext(FeedbackContext);
+  // console.log(value.feedback);
+  return !isLoadding && feedback.length === 0 ? 
+  (
+    <p>No Feedback Yet</p>
+  ) : isLoadding ? (
+    <Spinner />
+  ): (
     <div className="feedback-list">
+      <AnimatePresence>
       {/* <div>CatalogFeedback</div> */}
-      <FeedbackItem />
+      {feedback.map((item) => (
+        <motion.div key={item.id} initial={{ opacity : 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+          <FeedbackItem key={item.id} item={item} />
+        </motion.div>
+      ))}
+      </AnimatePresence>
     </div>
   );
 };
