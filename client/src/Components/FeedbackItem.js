e7d055c7c94c91cdf730300f437cedc382390c8f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-29 12:01:57 +0200

    Update FeedbackItem

--- client/src/Components/FeedbackItem.js
+++ client/src/Components/FeedbackItem.js
@@ -5,14 +5,14 @@ import Card from './shared/Card';
 const FeedbackItem = ({item}) => {
   return (
     <Card>
-    <div className="num-display">{}</div>
+    <div className="num-display">{item.rating}</div>
         <button type='button' onClick='' className='close'>
             <FaTimes color='purple' />
         </button>
         <button type='button' onClick='' className='edit'>
             <FaEdit color='purple'/>
         </button>
-        <div className="text-display">{}</div>
+        <div className="text-display">{item.text}</div>
         </Card>
   )
 }
