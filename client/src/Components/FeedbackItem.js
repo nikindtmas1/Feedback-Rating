a81676deb2d91077dc1549bd796f7bf39dc57a2d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-11-29 10:46:24 +0200

    Update FeedbacItem

--- client/src/Components/FeedbackItem.js
+++ client/src/Components/FeedbackItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { FaTimes, FaEdit } from 'react-icons/fa';
+import Card from './shared/Card';
 
-const FeedbackItem = () => {
+const FeedbackItem = ({item}) => {
   return (
-    <div className="num-display">
+    <Card>
+    <div className="num-display">{}</div>
         <button type='button' onClick='' className='close'>
             <FaTimes color='purple' />
         </button>
         <button type='button' onClick='' className='edit'>
-            <FaEdit />
+            <FaEdit color='purple'/>
         </button>
-    </div>
+        <div className="text-display">{}</div>
+        </Card>
   )
 }
 
