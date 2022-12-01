9b32069a74b1eecd8c27ceed20479b30890f020e
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 15:46:51 +0200

    Update PeopleSelect

--- client/src/Components/PeopleSelect.js
+++ client/src/Components/PeopleSelect.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import { FeedbackContext } from "../context/FeedbackContext";
+
+
 
 const SelectPeople = () => {
 
+    const {addName, fetchFeedback} = useContext(FeedbackContext);
+
+
+    
+   const onClick = (e) => {
+
+    addName('gosho')
+    fetchFeedback();
+   
+   
+   };
+
+
     return (
+        <>
         <select style={{width: 140, color: 'blue', borderRadius: "10px", padding: "3px"}}>
             <option style={{textAlign: "center"}}>Gosho</option>
             <option style={{textAlign: "center"}}>Tosho</option>
             <option style={{textAlign: "center"}}>Pesho</option>
             <option style={{textAlign: "center"}}>Tomi</option>
+            
         </select>
+        <button style={{borderRadius: '5px'}} type="button" onClick={onClick}>Submit</button>
+        </>
     );
 };
 
