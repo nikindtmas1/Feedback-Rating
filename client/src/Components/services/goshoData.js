65176bd381e47e35ca9928cec1d2f778981f35c2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 15:46:36 +0200

    Update goshodata

--- client/src/Components/services/goshoData.js
+++ client/src/Components/services/goshoData.js
@@ -5,7 +5,7 @@ const host = 'http://localhost:5000';
 service.settings.host = host;
 
 export async function getAll() {
-    let goshoFeedbacks = await service.get(host + '/goshoFeedbacks');
+    let goshoFeedbacks = await service.get(host + '/goshofeedbacks');
     let result = Object.values(goshoFeedbacks);
 
     return result;
