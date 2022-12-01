557982ba7b2cddb4f8802c8a8733e438c7c37e90
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 09:38:30 +0200

    Added css

--- client/src/Components/PeopleSelect.js
+++ client/src/Components/PeopleSelect.js
@@ -3,7 +3,7 @@ import React from "react";
 const SelectPeople = () => {
 
     return (
-        <select style={{width: 110, color: 'blue'}}>
+        <select style={{width: 140, color: 'blue', borderRadius: "10px", padding: "3px"}}>
             <option>Gosho</option>
             <option>Tosho</option>
             <option>Pesho</option>
