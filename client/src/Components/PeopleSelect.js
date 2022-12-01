6070c885737afb93e9dccb97bff0e09e020c069b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 16:49:28 +0200

    Update peopleSelect

--- client/src/Components/PeopleSelect.js
+++ client/src/Components/PeopleSelect.js
@@ -8,26 +8,30 @@ const SelectPeople = () => {
     const {addName, fetchFeedback} = useContext(FeedbackContext);
 
 
-    
-   const onClick = (e) => {
+    const onOptionChangeHandler = (e) => {
+        console.log(e.target.value);
+        // let currentName = e.target.value;
+        // addName(currentName);
 
-    addName('gosho')
-    fetchFeedback();
+    }
+//    const onClick = (e) => {
+//     addName('gosho')
+//     fetchFeedback();
    
    
-   };
+//    };
 
 
     return (
         <>
-        <select style={{width: 140, color: 'blue', borderRadius: "10px", padding: "3px"}}>
-            <option style={{textAlign: "center"}}>Gosho</option>
-            <option style={{textAlign: "center"}}>Tosho</option>
-            <option style={{textAlign: "center"}}>Pesho</option>
-            <option style={{textAlign: "center"}}>Tomi</option>
+        <select onChange={onOptionChangeHandler} style={{width: 140, color: 'blue', borderRadius: "10px", padding: "3px"}}>
+            <option value='gosho' style={{textAlign: "center"}}>Gosho</option>
+            <option value='tosho' style={{textAlign: "center"}}>Tosho</option>
+            <option value='pesho' style={{textAlign: "center"}}>Pesho</option>
+            <option value='tomi' style={{textAlign: "center"}}>Tomi</option>
             
         </select>
-        <button style={{borderRadius: '5px'}} type="button" onClick={onClick}>Submit</button>
+        {/* <button style={{borderRadius: '5px'}} type="button" onClick={onClick}>Submit</button> */}
         </>
     );
 };
