78e2a6dc1c44e3a86fd565558bf8438dd438f5da
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 09:44:17 +0200

    Added css

--- client/src/Components/PeopleSelect.js
+++ client/src/Components/PeopleSelect.js
@@ -4,10 +4,10 @@ const SelectPeople = () => {
 
     return (
         <select style={{width: 140, color: 'blue', borderRadius: "10px", padding: "3px"}}>
-            <option>Gosho</option>
-            <option>Tosho</option>
-            <option>Pesho</option>
-            <option>Tomi</option>
+            <option style={{textAlign: "center"}}>Gosho</option>
+            <option style={{textAlign: "center"}}>Tosho</option>
+            <option style={{textAlign: "center"}}>Pesho</option>
+            <option style={{textAlign: "center"}}>Tomi</option>
         </select>
     );
 };
