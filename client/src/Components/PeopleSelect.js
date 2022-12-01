21c9d2b1d0707f32691ed7c31da36babd2d26ee4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 09:34:36 +0200

    Create SelectPeople

---
+++ client/src/Components/PeopleSelect.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SelectPeople = () => {
+
+    return (
+        <select style={{width: 110, color: 'blue'}}>
+            <option>Gosho</option>
+            <option>Tosho</option>
+            <option>Pesho</option>
+            <option>Tomi</option>
+        </select>
+    );
+};
+
+export default SelectPeople;
