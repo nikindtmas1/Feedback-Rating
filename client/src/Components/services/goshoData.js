ac8efdb0368ec767b2304e57f375ed39fd92d1c6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 12:29:34 +0200

    Create gosho data

---
+++ client/src/Components/services/goshoData.js
@@ -0,0 +1,12 @@
+import * as service from './feedbackService';
+
+const host = 'http://localhost:5000';
+
+service.settings.host = host;
+
+export async function getAll() {
+    let goshoFeedbacks = await service.get(host + '/goshoFeedbacks');
+    let result = Object.values(goshoFeedbacks);
+
+    return result;
+}
