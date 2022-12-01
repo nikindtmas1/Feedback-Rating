8217ddc74718423d0a72b91b29d8b69854d68719
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-01 12:28:50 +0200

    Added route

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const feedbackController = require('../controllers/feedbackController');
+const goshoFeedbackController = require("../controllers/goshoFeedbackController");
 
 router.use('/feedbacks', feedbackController);
+router.use('/goshoFeedbacks', goshoFeedbackController);
 
 module.exports = router;
