60a5c3164a2b36639967af637f78fd6d1aefaff1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-02 16:30:39 +0200

    Update toshoController

--- server/src/controllers/toshoFeedbackController.js
+++ server/src/controllers/toshoFeedbackController.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const roter = express.Router();
+const router = express.Router();
 
 const services = require('../services/toshoFeedbackService');
-const router = require('./goshoFeedbackController');
 
 router.get('/', async (req, res) => {
     const toshoFeedbacks = await services.getAllToshoFeedback();
