8701c4c2f242cfdaba0fab422ce9e3b5780a82c6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-02 14:29:59 +0200

    Added tosho routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const feedbackController = require('../controllers/feedbackController');
 const goshoFeedbackController = require("../controllers/goshoFeedbackController");
+const toshoFeedbackController = require('../controllers/toshoFeedbackController');
 
 router.use('/feedbacks', feedbackController);
 router.use('/goshofeedbacks', goshoFeedbackController);
+router.use('/toshofeedbacks', toshoFeedbackController);
 
 module.exports = router;
