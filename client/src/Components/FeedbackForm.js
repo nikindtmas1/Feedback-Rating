82c9012fa0be1f6aeeedca013f9b5ddbf9e0a3a3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-02 16:35:07 +0200

    Change form

--- client/src/Components/FeedbackForm.js
+++ client/src/Components/FeedbackForm.js
@@ -55,7 +55,7 @@ const FeedbackForm = () => {
     <div className="text-container">
     <div className="text-content">
     <form onSubmit={handleSubmit}>
-                <h2>How would you rate therapist <SelectPeople /> ?</h2>
+                <h2>How would you rate therapist ?<SelectPeople /> </h2>
                 <RatingSelect selected={rating} select={(rating) => setRating(rating)} />
                 <div className="input-group">
                     <input type="text" name='text' placeholder="Write a review" value={text} onChange={handleTextChange} />
