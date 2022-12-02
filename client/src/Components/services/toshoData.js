4151c6bb8de58970cdac03bc9b0f0c85a05b1ff7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-02 16:30:58 +0200

    Update toshoData

--- client/src/Components/services/toshoData.js
+++ client/src/Components/services/toshoData.js
@@ -5,7 +5,7 @@ const host = 'http://localhost:5000';
 service.settings.host = host;
 
 export async function getAll() {
-    let toshoFeedbacks = service.get(host + '/toshofeedbacks');
+    let toshoFeedbacks = await service.get(host + '/toshofeedbacks');
     let result = Object.values(toshoFeedbacks);
 
     return result;
