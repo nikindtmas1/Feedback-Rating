4423eddabcc765ade58cf1ac2767a07cc23ba28b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-05 11:22:31 +0200

    Added create function

--- server/src/services/goshoFeedbackService.js
+++ server/src/services/goshoFeedbackService.js
@@ -1,8 +1,11 @@
 const GoshoFeedback = require('../models/goshoFeedbackModel');
 
 const getAllGoshoFeedback = () => GoshoFeedback.find();
+const createGoshoFeedback = (data) => GoshoFeedback.create(data);
 
 module.exports = {
     getAllGoshoFeedback,
+    createGoshoFeedback,
+    
     
 }
