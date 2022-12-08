34d72628e3494f07f4366c6cf27694bb1d0e2957
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-08 10:59:28 +0200

    Format page

--- client/src/context/FeedbackContext.js
+++ client/src/context/FeedbackContext.js
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
-import * as services from '../Components/services/data';
-import * as goshoServices from '../Components/services/goshoData';
-import * as toshoServices from '../Components/services/toshoData';
+import * as services from "../Components/services/data";
+import * as goshoServices from "../Components/services/goshoData";
+import * as toshoServices from "../Components/services/toshoData";
 //const host = "http://localhost:5000";
 
 export const FeedbackContext = createContext();
@@ -10,98 +10,97 @@ export const FeedbackProvider = ({ children }) => {
   const [isLoadding, setIsLoading] = useState(true);
   const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState(false);
-  const [peopleName, setPeopleName] = useState('');
-
+  const [peopleName, setPeopleName] = useState("");
 
   useEffect(() => {
     fetchFeedback();
   }, []);
 
-  const fetchFeedback =  () => {
-
-      services.getAll()
+  const fetchFeedback = () => {
+    services
+      .getAll()
       .then((result) => setFeedback(result))
       .then(() => setIsLoading(false))
-   
-      .then((err) => alert(err.massage))
-    
-  
+
+      .then((err) => alert(err.massage));
   };
 
   const feedbackByName = (name) => {
-
-    if(name === 'gosho'){
-      goshoServices.getAll()
-      .then((result) => setFeedback(result))
-      .then(() => setPeopleName(name))
-      .then((err) => alert(err.massage))
-    }else if(name === 'tosho'){
-      toshoServices.getAll()
-      .then((result) => setFeedback(result))
-      .then((err) => alert(err.massage))
-    }else if(name !== 'gosho' && name!== 'tosho' && name !== 'pesho' && name !== 'tomi'){
+    if (name === "gosho") {
+      goshoServices
+        .getAll()
+        .then((result) => setFeedback(result))
+        .then(() => setPeopleName(name))
+        .then((err) => alert(err.massage));
+    } else if (name === "tosho") {
+      toshoServices
+        .getAll()
+        .then((result) => setFeedback(result))
+        .then((err) => alert(err.massage));
+    } else if (
+      name !== "gosho" &&
+      name !== "tosho" &&
+      name !== "pesho" &&
+      name !== "tomi"
+    ) {
       fetchFeedback();
     }
   };
 
-  
-
   const addFeedback = (data) => {
-    if(peopleName === 'gosho'){
-      goshoServices.createGoshoFeedback(data)
-      .then(() => {
-        goshoServices.getAll()
-        .then((result) => setFeedback(result))
-      })
-      .then((err) => alert(err.massage));
-    }else{
-
-      services.createFeedback(data)
-      .then(() => {
-        services.getAll()
-        .then((result) => setFeedback(result))
-       
-      })
-      .then((err) => alert(err.massage))
+    if (peopleName === "gosho") {
+      goshoServices
+        .createGoshoFeedback(data)
+        .then(() => {
+          goshoServices.getAll().then((result) => setFeedback(result));
+        })
+        .then((err) => alert(err.massage));
+    } else {
+      services
+        .createFeedback(data)
+        .then(() => {
+          services.getAll().then((result) => setFeedback(result));
+        })
+        .then((err) => alert(err.massage));
     }
   };
 
- 
-
   const removeFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete?')){
-      if(peopleName === 'gosho'){
-        goshoServices.deleteGoshoFeedback(id)
+    if (window.confirm("Are you sure you want to delete?")) {
+      if (peopleName === "gosho") {
+        goshoServices
+          .deleteGoshoFeedback(id)
+          .then(() => {
+            const newFeedback = feedback.filter((item) => item._id !== id);
+
+            setFeedback(newFeedback);
+          })
+          .then((err) => alert(err.message));
+      } else {
+      }
+      services
+        .deleteFeedback(id)
         .then(() => {
-          const newFeedback = feedback.filter((item) => item._id !== id)
+          const newFeedback = feedback.filter((item) => item._id !== id);
 
           setFeedback(newFeedback);
         })
         .then((err) => alert(err.message));
-      }else{
-
-      }
-      services.deleteFeedback(id)
-      .then(() => {
-        const newFeedback = feedback.filter((item) => item._id !== id)
-
-        setFeedback(newFeedback);
-      })
-      .then((err) => alert(err.message))
-      
     }
   };
 
   return (
-    <FeedbackContext.Provider value={{ 
-      feedback, 
-      isLoadding, 
-      addFeedback, 
-      removeFeedback, 
-      feedbackByName, 
-      peopleName,
-      fetchFeedback 
-      }}>
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        isLoadding,
+        addFeedback,
+        removeFeedback,
+        feedbackByName,
+        peopleName,
+        fetchFeedback,
+      }}
+    >
       {children}
     </FeedbackContext.Provider>
   );
