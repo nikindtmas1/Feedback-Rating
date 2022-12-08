a25d55fe06a5767ff6ab44c3eeb71aa4c2c1d157
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-08 13:29:04 +0200

    Added peopleName

--- client/src/Components/FeedbackItem.js
+++ client/src/Components/FeedbackItem.js
@@ -1,25 +1,28 @@
-import React, {useContext} from 'react';
-import { FaTimes, FaEdit } from 'react-icons/fa';
-import { FeedbackContext } from '../context/FeedbackContext';
-import Card from './shared/Card';
+import React, { useContext } from "react";
+import { FaTimes, FaEdit } from "react-icons/fa";
+import { FeedbackContext } from "../context/FeedbackContext";
+import Card from "./shared/Card";
 
-const FeedbackItem = ({item}) => {
-
+const FeedbackItem = ({ item }) => {
   const { removeFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
-    <div className="num-display">{item.rating}</div>
-        <button type='button' onClick={() => removeFeedback(item._id)} className='close'>
-            <FaTimes color='red' />
-        </button>
-        <button type='button' onClick='' className='edit'>
-            <FaEdit color='purple'/>
-        </button>
-        <div className="text-display">{item.text}</div>
-        {/* <div className='text-display'>{name.peopleName}</div> */}
-        </Card>
-  )
-}
+      <div className="num-display">{item.rating}</div>
+      <button
+        type="button"
+        onClick={() => removeFeedback(item._id)}
+        className="close"
+      >
+        <FaTimes color="red" />
+      </button>
+      <button type="button" onClick="" className="edit">
+        <FaEdit color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+      <div className="name-text">{item.peopleName}</div>
+    </Card>
+  );
+};
 
-export default FeedbackItem
+export default FeedbackItem;
