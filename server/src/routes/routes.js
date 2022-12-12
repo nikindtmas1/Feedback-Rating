d3d475c2a8bc51c12f18e5a34acb564fdd893253
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-12 15:01:11 +0200

    Added pesho controller

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 const feedbackController = require('../controllers/feedbackController');
 const goshoFeedbackController = require("../controllers/goshoFeedbackController");
 const toshoFeedbackController = require('../controllers/toshoFeedbackController');
+const peshoFeedbackController = require('../controllers/peshoFeedbackController');
+
 
 router.use('/feedbacks', feedbackController);
 router.use('/goshofeedbacks', goshoFeedbackController);
 router.use('/toshofeedbacks', toshoFeedbackController);
+router.use('/peshofeedbacks', peshoFeedbackController);
 
 module.exports = router;
