1130be3158c442de98b034f3746ffdfcbf48fd57
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-12 10:48:24 +0200

    Added delete service

--- server/src/services/toshoFeedbackService.js
+++ server/src/services/toshoFeedbackService.js
@@ -2,9 +2,10 @@ const ToshoFeedback = require('../models/toshoFeedbackModel');
 
 const getAllToshoFeedback = () => ToshoFeedback.find();
 const createToshoFeedback = (data) => ToshoFeedback.create(data);
+const deleteToshoFeedback = (id) => ToshoFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllToshoFeedback,
     createToshoFeedback,
-
+    deleteToshoFeedback,
 }
