8ce2d475f27b0c68760b390289e27bcc46be301d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-12 10:06:58 +0200

    Create ToshoFeedback service

--- server/src/services/toshoFeedbackService.js
+++ server/src/services/toshoFeedbackService.js
@@ -1,7 +1,10 @@
 const ToshoFeedback = require('../models/toshoFeedbackModel');
 
 const getAllToshoFeedback = () => ToshoFeedback.find();
+const createToshoFeedback = (data) => ToshoFeedback.create(data);
 
 module.exports = {
     getAllToshoFeedback,
+    createToshoFeedback,
+
 }
