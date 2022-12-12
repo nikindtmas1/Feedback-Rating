89dd8d8f854653b8bb7e0ae6c69a8775f5b3b10c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-12 15:28:00 +0200

    Create peshoData

---
+++ client/src/Components/services/peshoData.js
@@ -0,0 +1,12 @@
+import * as service from '../services/feedbackService';
+
+const host = 'http://localhost:5000';
+
+service.settings.host = host;
+
+export async function getAll() {
+    let peshoFeedbacks = await service.get(host + '/peshofeedbacks');
+    let result = Object.values(peshoFeedbacks);
+
+    return result;
+};
