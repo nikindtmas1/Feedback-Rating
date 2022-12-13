af280f430b3a63bd97e172ea68d4746719dc7d27
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-13 15:46:53 +0200

    Added createPeshoFeedback

--- server/src/services/peshoFeedbackService.js
+++ server/src/services/peshoFeedbackService.js
@@ -1,7 +1,9 @@
 const PeshoFeedback = require('../models/peshoFeedbackModel');
 
 const getAllPeshoFeedback = () => PeshoFeedback.find();
+const createPeshoFeedback = (data) => PeshoFeedback.create(data);
 
 module.exports = {
     getAllPeshoFeedback,
+    createPeshoFeedback,
 };
