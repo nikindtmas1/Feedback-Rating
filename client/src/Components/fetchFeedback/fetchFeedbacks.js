c0653a84e99c386e96ba3ca9435bbe02989d0dc6
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-13 16:34:23 +0200

    Formated

--- client/src/Components/fetchFeedback/fetchFeedbacks.js
+++ client/src/Components/fetchFeedback/fetchFeedbacks.js
@@ -1,20 +1,17 @@
-import * as service from '../services/data';
-import * as goshoService from '../services/goshoData';
-import * as toshoService from '../services/toshoData';
-import * as peshoService from '../services/peshoData';
+import * as service from "../services/data";
+import * as goshoService from "../services/goshoData";
+import * as toshoService from "../services/toshoData";
+import * as peshoService from "../services/peshoData";
 
-export function fetchByName (name) {
-
-    if(name === 'Gosho'){
-       return goshoService.getAll();
-    }else if(name === 'Tosho'){
-        return toshoService.getAll();
-    }else if(name === 'Pesho'){
-        return peshoService.getAll();
-    }else if(name === 'Tomi'){
-
-    }else if(name === 'start'){
-        return service.getAll();
-    }
-
-};
+export function fetchByName(name) {
+  if (name === "Gosho") {
+    return goshoService.getAll();
+  } else if (name === "Tosho") {
+    return toshoService.getAll();
+  } else if (name === "Pesho") {
+    return peshoService.getAll();
+  } else if (name === "Tomi") {
+  } else if (name === "start") {
+    return service.getAll();
+  }
+}
