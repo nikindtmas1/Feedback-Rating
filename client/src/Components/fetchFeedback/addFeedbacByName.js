132e647ce0bcd110587a88f015e1e756918660c5
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-13 16:34:31 +0200

    Formated

--- client/src/Components/fetchFeedback/addFeedbacByName.js
+++ client/src/Components/fetchFeedback/addFeedbacByName.js
@@ -1,18 +1,19 @@
-import * as goshoService from '../services/goshoData';
-import * as toshoService from '../services/toshoData';
-import * as peshoService from '../services/peshoData';
-
+import * as goshoService from "../services/goshoData";
+import * as toshoService from "../services/toshoData";
+import * as peshoService from "../services/peshoData";
 
 export function addFeedbackByName(data) {
-    
-    if(data.peopleName === 'Gosho'){
-      return goshoService.createGoshoFeedback(data)
+  if (data.peopleName === "Gosho") {
+    return goshoService
+      .createGoshoFeedback(data)
       .then(() => goshoService.getAll());
-    } else if(data.peopleName === 'Tosho'){
-        return toshoService.createToshoFeedback(data)
-        .then(() => toshoService.getAll());
-    }else if(data.peopleName === 'Pesho'){
-        return peshoService.createPeshoFeedback(data)
-        .then(() => peshoService.getAll());
-    }
+  } else if (data.peopleName === "Tosho") {
+    return toshoService
+      .createToshoFeedback(data)
+      .then(() => toshoService.getAll());
+  } else if (data.peopleName === "Pesho") {
+    return peshoService
+      .createPeshoFeedback(data)
+      .then(() => peshoService.getAll());
+  }
 }
