9b6c64af99670d0bcef5d36863a5d291c6cc51d1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-14 10:50:45 +0200

    Added dleteFeedbackService

--- server/src/services/peshoFeedbackService.js
+++ server/src/services/peshoFeedbackService.js
@@ -2,8 +2,10 @@ const PeshoFeedback = require('../models/peshoFeedbackModel');
 
 const getAllPeshoFeedback = () => PeshoFeedback.find();
 const createPeshoFeedback = (data) => PeshoFeedback.create(data);
+const deletePeshoFeedback = (id) => PeshoFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllPeshoFeedback,
     createPeshoFeedback,
+    deletePeshoFeedback,
 };
