fa618699ff12a5cbc636b7634d4b0260686f16a2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-14 11:30:21 +0200

    Import tomiFeedbackController

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -4,11 +4,13 @@ const feedbackController = require('../controllers/feedbackController');
 const goshoFeedbackController = require("../controllers/goshoFeedbackController");
 const toshoFeedbackController = require('../controllers/toshoFeedbackController');
 const peshoFeedbackController = require('../controllers/peshoFeedbackController');
+const tomiFeedbackController = require('../controllers/tomiFeedbackController');
 
 
 router.use('/feedbacks', feedbackController);
 router.use('/goshofeedbacks', goshoFeedbackController);
 router.use('/toshofeedbacks', toshoFeedbackController);
 router.use('/peshofeedbacks', peshoFeedbackController);
+router.use('/tomifeedbacks', tomiFeedbackController);
 
 module.exports = router;
