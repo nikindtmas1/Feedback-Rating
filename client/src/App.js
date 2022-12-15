a62d5f99819db3591f77bc951f4c0808ca19cea4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 13:22:07 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,23 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-import CatalogFeedback from "./Components/CatalogFeedback";
-import FeedbackForm from "./Components/FeedbackForm";
 import { FeedbackProvider } from "./context/FeedbackContext";
 import AboutIconLink from "./Components/AboutIconLink";
-import FeedbackStats from "./Components/FeedbackStats";
 import Header from "./Components/Header";
+import LoginPage from "./Components/pages/LoginPage";
+import Dashboard from "./Components/pages/Dashboard";
 
 function App() {
   return (
     <FeedbackProvider>
-      <BrowserRouter>
-           <Header />
-        <div className="App">
-          <header className="App-header">
-            {/* <p>Health&Relax Center Feedbacks</p> */}
-            <FeedbackForm />
-            <FeedbackStats />
-            <CatalogFeedback />
-          </header>
-        </div>
-        <AboutIconLink />
-      </BrowserRouter>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/login" component={LoginPage} />
+        </header>
+      </div>
+      <AboutIconLink />
     </FeedbackProvider>
   );
 }
