45234003dac11dff33cf614015be8f57c6925ed7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 13:21:40 +0200

    Update Header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,11 +3,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <Navbar>
-      <div>
-        <h2>Health & Relax Center</h2>
-      </div>
-    </Navbar>
+    <Navbar />
   );
 };
 
