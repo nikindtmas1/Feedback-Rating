bba6ab4d5abfbb7e38fdcc671f434b03cf9449d0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 15:49:51 +0200

    Update AboutLink

--- client/src/Components/AboutIconLink.js
+++ client/src/Components/AboutIconLink.js
@@ -1,11 +1,14 @@
-import {FaQuestion} from 'react-icons/fa';
+// import {FaQuestion} from 'react-icons/fa';
+// import { FaSistrix } from 'react-icons/fa';
+import { FaLightbulb } from 'react-icons/fa';
 import  {Link} from 'react-router-dom';
 
 const AboutIconLink = () => {
     return (
         <div className='about-link'>
             <Link to = {'/login'}>
-                <FaQuestion size={30} />            
+                {/* <FaQuestion size={30} />   */}
+                       <FaLightbulb size={30} />
             </Link>
         </div>
     );
