6a58a9e6c10b807e3f7eee833b4c12230e1e6764
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 13:43:31 +0200

    Update Logon Page

--- client/src/Components/pages/LoginPage.js
+++ client/src/Components/pages/LoginPage.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "../shared/Card";
+import Button from "../shared/Button";
 
 const loginPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <div>loginPage</div>
-      <div>
-        <Link to="/">Home Page</Link>
+    <Card>
+      <div className="text-container">
+        <div className="text-content">
+          <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <div className="input-group">
+              <input type="text" name="userLogin" placeholder="User Name" />
+            </div>
+            <div className="input-group">
+              <input type="password" name="password" placeholder="Password" />
+            </div>
+            <Button type="submit" isDisabled={false}>
+              Submit
+            </Button>
+            <Link to="/">Home Page</Link>
+          </form>
+        </div>
       </div>
-    </div>
+    </Card>
   );
 };
 
