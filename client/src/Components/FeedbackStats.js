598f72f3f870a6fb1ca80937d8ce24f0fdfcd374
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 13:21:56 +0200

    Update Feedback State

--- client/src/Components/FeedbackStats.js
+++ client/src/Components/FeedbackStats.js
@@ -14,7 +14,7 @@ const FeedbackStats = () => {
     return (
         <div className="feedback-stats">
             <h4>{feedback.length} Reviews</h4>
-            <> - </>
+            {/* <>  -  </> */}
             <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
         </div>
     );
