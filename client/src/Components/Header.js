d6c3676b9075262fa652f484fbea77d41a9e78b7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 11:25:53 +0200

    Create Header

---
+++ client/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <Navbar>
+      <div>
+        <h2>Health & Relax Center</h2>
+      </div>
+    </Navbar>
+  );
+};
+
+export default Header;
