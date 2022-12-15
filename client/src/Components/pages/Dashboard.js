05724a14b9024fc65c96d6cb7f8ba25ab89f99d9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 14:09:46 +0200

    Import About link

--- client/src/Components/pages/Dashboard.js
+++ client/src/Components/pages/Dashboard.js
@@ -2,14 +2,18 @@ import React from "react";
 import FeedbackForm from "../FeedbackForm";
 import FeedbackStats from "../FeedbackStats";
 import CatalogFeedback from "../CatalogFeedback";
+import AboutIconLink from "../AboutIconLink";
 
 const Dashboard = () => {
   return (
-    <div>
-      <FeedbackForm />
-      <FeedbackStats />
-      <CatalogFeedback />
-    </div>
+    <>
+      <div>
+        <FeedbackForm />
+        <FeedbackStats />
+        <CatalogFeedback />
+      </div>
+      <AboutIconLink />
+    </>
   );
 };
 
