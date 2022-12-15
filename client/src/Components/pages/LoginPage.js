cb20d1c530da853fd1bca68f4652f132703542db
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 15:18:21 +0200

    Update Login Page

--- client/src/Components/pages/LoginPage.js
+++ client/src/Components/pages/LoginPage.js
@@ -1,19 +1,24 @@
-import React, {useContext} from "react";
-import { useHistory } from "react-router-dom";
+import React, { useContext } from "react";
+import { useHistory, Link } from "react-router-dom";
 import Card from "../shared/Card";
 import { FeedbackContext } from "../../context/FeedbackContext";
-
 
 const LoginPage = () => {
 
   const history = useHistory();
-  const {feedbackByName} = useContext(FeedbackContext);
-  const peopleName = 'start';
+  const { feedbackByName } = useContext(FeedbackContext);
+  const peopleName = "start";
 
   const handleSubmit = (e) => {
     e.preventDefault();
     feedbackByName(peopleName);
-    history.push('/');
+    history.push("/");
+  };
+
+  const onClick = (e) => {
+    e.preventDefault();
+    feedbackByName(peopleName);
+    history.push("/");
   };
 
   return (
@@ -21,28 +26,38 @@ const LoginPage = () => {
       <div className="text-container">
         <div className="text-content">
           <form onSubmit={handleSubmit}>
-            <h2>Login</h2>
+            <h2   style={{
+            color: 'rgb(45, 179, 206)'
+          }}>Login</h2>
             <div className="input-group">
               <input type="text" name="userLogin" placeholder="User Name" />
             </div>
             <div className="input-group">
               <input type="password" name="password" placeholder="Password" />
             </div>
-            <div className="input-group">
+            <div className="button-login">
               <button 
-              type="submit"
-              style={{
-                padding: '10px',
-                margin: '10px',
-                width: '500px',
-                backgroundColor: 'rgb(45, 179, 206)',
-                color: 'white',
-                borderRadius: '10px'
-              }}
-              >Submit</button>
+                type="submit"
+                style={{
+                  padding: "10px",
+                  margin: "10px",
+                  width: "500px",
+                  backgroundColor: "rgb(105, 179, 206)",
+                  color: "white",
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                }}
+              >
+                Submit
+              </button>
             </div>
           </form>
         </div>
+          <Link onClick={onClick} to='/'
+          style={{
+            color: 'rgb(45, 179, 206)'
+          }}
+          >Back To Home</Link>
       </div>
     </Card>
   );
