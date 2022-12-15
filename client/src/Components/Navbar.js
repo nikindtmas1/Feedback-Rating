4d165bd27d6df76cf34009a3985a4ed5f7d64036
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 11:25:37 +0200

    Create Navbar

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div style={{
+        backgroundColor: "rgb(45, 179, 206)",
+        color: "blue",
+        textAlign: "center"
+    }}>
+        <h3 style={{ margin: "0"}}>
+
+        Health & Relax Center
+        </h3>
+    </div>
+  )
+}
+
+export default Navbar
