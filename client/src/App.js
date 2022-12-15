ab5cb4e9cfadb5747bcda1ffba53a1f547778b21
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 11:26:08 +0200

    Import Header

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,16 @@ import FeedbackForm from "./Components/FeedbackForm";
 import { FeedbackProvider } from "./context/FeedbackContext";
 import AboutIconLink from "./Components/AboutIconLink";
 import FeedbackStats from "./Components/FeedbackStats";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <FeedbackProvider>
       <BrowserRouter>
+           <Header />
         <div className="App">
           <header className="App-header">
-            <p>Health&Relax Center Feedbacks</p>
+            {/* <p>Health&Relax Center Feedbacks</p> */}
             <FeedbackForm />
             <FeedbackStats />
             <CatalogFeedback />
