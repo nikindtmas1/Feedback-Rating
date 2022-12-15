aeaae6dba51fafddc028529e57c5148625a38c22
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-15 13:21:01 +0200

    Added link to home page

--- client/src/Components/pages/LoginPage.js
+++ client/src/Components/pages/LoginPage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const loginPage = () => {
   return (
-    <div>loginPage</div>
-  )
-}
+    <div>
+      <div>loginPage</div>
+      <div>
+        <Link to="/">Home Page</Link>
+      </div>
+    </div>
+  );
+};
 
-export default loginPage
+export default loginPage;
