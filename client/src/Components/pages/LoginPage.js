1768749b728227445b8b8ed27338640d4e4c4a0d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-16 11:09:58 +0200

    Update LoginPage

--- client/src/Components/pages/LoginPage.js
+++ client/src/Components/pages/LoginPage.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Card from "../shared/Card";
 import { FeedbackContext } from "../../context/FeedbackContext";
-import Button from "../shared/Button";
+import Login from "../shared/Login";
 
 const LoginPage = () => {
   const history = useHistory();
@@ -11,50 +11,28 @@ const LoginPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let formData = new FormData(e.currentTarget);
+    let nameUser = formData.get('userLogin').trim();
+    let password = formData.get('password').trim();
+
+    let userData = {
+      nameUser,
+      password,
+    };
+
+    console.log(userData);
+
     feedbackByName(peopleName);
     history.push("/");
   };
 
-  const onClick = (e) => {
-    e.preventDefault();
-    feedbackByName(peopleName);
-    history.push("/");
-  };
 
   return (
     <Card>
       <div className="text-container">
         <div className="text-content">
           <form onSubmit={handleSubmit}>
-            <h2
-              style={{
-                color: "rgb(45, 179, 206)",
-              }}
-            >
-              Login
-            </h2>
-            <div className="input-group">
-              <input type="text" name="userLogin" placeholder="User Name" />
-            </div>
-            <div className="input-group">
-              <input type="password" name="password" placeholder="Password" />
-            </div>
-            <div className="button-login">
-              <Button type="submit" disabled="false" version="tertiary">
-                Submit
-              </Button>
-            </div>
-            <Button type="submit" version="secondary">
-              <Link
-                onClick={onClick}
-                to="/"
-                style={{
-                  color: "blue",
-                }}
-              >
-                Back
-              </Link>
-            </Button>
+            <Login />
           </form>
         </div>
       </div>
