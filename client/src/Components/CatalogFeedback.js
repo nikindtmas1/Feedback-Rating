9fa2dd24ca3c0a998f2027338c342b9ad72ca4d1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-16 12:04:56 +0200

    Update catalog feedback

--- client/src/Components/CatalogFeedback.js
+++ client/src/Components/CatalogFeedback.js
@@ -6,8 +6,7 @@ import Spinner from '../Components/shared/Spinner';
 
 const CatalogFeedback = () => {
   const { feedback, isLoadding } = useContext(FeedbackContext);
-  // let value = useContext(FeedbackContext);
-  // console.log(value.feedback);
+ 
   return !isLoadding && feedback.length === 0 ? 
   (
     <p>No Feedback Yet</p>
@@ -16,7 +15,6 @@ const CatalogFeedback = () => {
   ): (
     <div className="feedback-list">
       <AnimatePresence>
-      {/* <div>CatalogFeedback</div> */}
       {feedback.map((item) => (
         <motion.div key={item.id} initial={{ opacity : 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
           <FeedbackItem key={item.id} item={item} />
