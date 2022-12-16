deaf3ca173798923d81a8fc9e27fbcd13b44a610
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-16 11:09:44 +0200

    Create Login

---
+++ client/src/Components/shared/Login.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "./Button";
+
+const Login = () => {
+  return (
+    <>
+      <h2
+        style={{
+          color: "rgb(45, 179, 206)",
+        }}
+      >
+        Login
+      </h2>
+      <div className="input-group">
+        <input type="text" name="userLogin" placeholder="User Name" />
+      </div>
+      <div className="input-group">
+        <input type="password" name="password" placeholder="Password" />
+      </div>
+      <div className="button-login">
+        <Button type="submit" disabled="false" version="tertiary">
+          Submit
+        </Button>
+      </div>
+      <Button type="submit" version="secondary">
+        Back
+      </Button>
+    </>
+  );
+};
+
+export default Login;
