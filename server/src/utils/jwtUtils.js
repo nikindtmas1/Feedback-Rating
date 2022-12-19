ac426e4f78f205a3c7172af075a6968bd9be7009
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-19 11:47:28 +0200

    Create utils.js

---
+++ server/src/utils/jwtUtils.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const { development } = require('../config/config');
+
+const secretAccess = development.secretStr;
+
+async function createAccessToken(user) {
+    let payload = {
+        _id: user._id,
+        username: user.username
+    };
+
+    let options = {
+        expiresIn: '2d'
+    };
+
+    let accessToken = await jwt.sign(payload, secretAccess,options);
+
+    return accessToken;
+};
+
+async function createRefreshToken(user) {
+    let payload = {
+        _id: user._id
+    };
+
+    let options = {
+        expiresIn: '2d'
+    };
+
+    let secretStrTwo = 'mnogoqkaparola2';
+
+    let refreshToken = await jwt.sign(payload,secretStrTwo,options);
+
+    return refreshToken;
+
+};
+
+module.exports = {
+    createAccessToken,
+    createRefreshToken,
+};
