c0a3d6656e79bf37cc93060698f957f75248d1df
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-19 11:47:42 +0200

    Create userLoginModel

---
+++ server/src/models/userLoginModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+});
+
+userSchema.static('findByUsername', function (username) {
+   return this.findOne({username});
+});
+
+module.exports = mongoose.model("UserLogin", userSchema);
