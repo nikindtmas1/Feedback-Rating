608a86d3682029fa52e7526150a277830380f167
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-19 14:32:44 +0200

    Added user router

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -5,6 +5,7 @@ const goshoFeedbackController = require("../controllers/goshoFeedbackController"
 const toshoFeedbackController = require('../controllers/toshoFeedbackController');
 const peshoFeedbackController = require('../controllers/peshoFeedbackController');
 const tomiFeedbackController = require('../controllers/tomiFeedbackController');
+const userController = require('../controllers/userController');
 
 
 router.use('/feedbacks', feedbackController);
@@ -12,5 +13,7 @@ router.use('/goshofeedbacks', goshoFeedbackController);
 router.use('/toshofeedbacks', toshoFeedbackController);
 router.use('/peshofeedbacks', peshoFeedbackController);
 router.use('/tomifeedbacks', tomiFeedbackController);
+router.use('/users', userController);
+
 
 module.exports = router;
