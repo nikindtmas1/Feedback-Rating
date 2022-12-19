bf678a20652507f0060202d92438806b09c93210
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-19 14:33:16 +0200

    Added login and logout

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const service = require('../services/userService');
+const { isGuest } = require('../middleware/authMidd');
+
+
+router.post('/login',isGuest, async (req, res) => {
+    let {username, password} = req.body;
+
+   let {user, accessToken, refreshToken} = await service.login({username, password});
+
+    res.json({
+        _id: user._id,
+        username: user.username,
+        accessToken,
+        refreshToken,
+    });
+});
+
+router.get('/logout', (req, res) => {
+    res.json({ ok: true });
+  });
+
+module.exports = router;
