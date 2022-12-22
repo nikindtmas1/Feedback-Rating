fa91932d92c449bda0b898ebbfc0387b3800fb75
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-22 14:04:55 +0200

    Formated

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import { useContext } from 'react';
-import { FeedbackContext } from '../context/FeedbackContext';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { useContext } from "react";
+import { FeedbackContext } from "../context/FeedbackContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
-  const {isAuth} = useContext(FeedbackContext);
+  const { isAuth } = useContext(FeedbackContext);
 
   return (
-    <div style={{
+    <div
+      style={{
         // backgroundColor: "rgb(45, 179, 206)",
-        backgroundColor:"rgb(69, 201, 253)",
+        backgroundColor: "rgb(69, 201, 253)",
         color: "blue",
         textAlign: "center",
-        padding: '4px'
-    }}>
-        <h2 style={{ margin: "0"}}>
-
-        Health & Relax Center
-        </h2>
-        {isAuth ? (
-          <Link to='/logout' style={{
-            display: 'flex',
-            textDecoration: 'none'
-            }}>Logout</Link>
-
-        ) : (null)}
+        padding: "4px",
+      }}
+    >
+      <h2 style={{ margin: "0" }}>Health & Relax Center</h2>
+      {isAuth ? (
+        <Link
+          to="/logout"
+          style={{
+            display: "flex",
+            textDecoration: "none",
+          }}
+        >
+          Logout
+        </Link>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
