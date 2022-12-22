5fdbbbc965d352b512f9024d016270d8f46adcf0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-22 14:30:32 +0200

    Added isAuth

--- client/src/Components/FeedbackItem.js
+++ client/src/Components/FeedbackItem.js
@@ -4,21 +4,25 @@ import { FeedbackContext } from "../context/FeedbackContext";
 import Card from "./shared/Card";
 
 const FeedbackItem = ({ item }) => {
-  const { removeFeedback } = useContext(FeedbackContext);
+  const { removeFeedback, isAuth } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button
-        type="button"
-        onClick={() => removeFeedback(item._id)}
-        className="close"
-      >
-        <FaTimes color="red" />
-      </button>
-      <button type="button" onClick="" className="edit">
-        <FaEdit color="purple" />
-      </button>
+      {isAuth ? (
+        <>
+          <button
+            type="button"
+            onClick={() => removeFeedback(item._id)}
+            className="close"
+          >
+            <FaTimes color="red" />
+          </button>
+          <button type="button" onClick="" className="edit">
+            <FaEdit color="purple" />
+          </button>
+        </>
+      ) : null}
       <div className="text-display">{item.text}</div>
       <div className="name-text">{item.peopleName}</div>
       <div className="name-user">{item.userName}</div>
