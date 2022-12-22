c46f9b1f7c6deb621ac655c91b6cb84103ab65ef
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-22 14:04:40 +0200

    Formated

--- client/src/Components/pages/Logout.js
+++ client/src/Components/pages/Logout.js
@@ -1,24 +1,21 @@
-import {useEffect, useContext} from 'react';
-import { FeedbackContext } from '../../context/FeedbackContext';
-import { useHistory } from 'react-router-dom';
-import * as service from '../services/data';
+import { useEffect, useContext } from "react";
+import { FeedbackContext } from "../../context/FeedbackContext";
+import { useHistory } from "react-router-dom";
+import * as service from "../services/data";
 
 const Logout = () => {
-    const history = useHistory();
-    const { feedbackByName, onLogout } = useContext(FeedbackContext);
-    const peopleName = "start";
+  const history = useHistory();
+  const { feedbackByName, onLogout } = useContext(FeedbackContext);
+  const peopleName = "start";
 
-    useEffect(() => {
-
-        service.logout()
-        .then(() => feedbackByName(peopleName))
-        .then(() => 
-        onLogout(),
-        history.push('/')
-        )
-    },[peopleName,history, feedbackByName, onLogout]);
+  useEffect(() => {
+    service
+      .logout()
+      .then(() => feedbackByName(peopleName))
+      .then(() => onLogout(), history.push("/"));
+  }, [peopleName, history, feedbackByName, onLogout]);
 
   return null;
-}
+};
 
-export default Logout
+export default Logout;
