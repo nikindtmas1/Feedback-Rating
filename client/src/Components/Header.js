48cf63f17a41bf469df740d6b722b2fe91d025d9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-22 16:17:40 +0200

    Update header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <Navbar />
+    <div className="App-header">
+    <h2 
+    style={{ 
+      margin: "0",
+      backgroundColor: "rgb(69, 201, 253)",
+      color: "blue",
+      textAlign: "center",
+      padding: "4px",
+       }}
+       >Health & Relax Center</h2>
+    </div>
   );
 };
 
