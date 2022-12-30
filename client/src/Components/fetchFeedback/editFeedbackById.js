cc0f0af253dbbf594e5e470a46aba96041d1ee15
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-12-30 09:21:48 +0200

    Added candition for Gosho

--- client/src/Components/fetchFeedback/editFeedbackById.js
+++ client/src/Components/fetchFeedback/editFeedbackById.js
@@ -1,4 +1,8 @@
+import * as goshoServices from '../services/goshoData';
 
 export function editFeedbackById(id, peopleName, feedback) {
-    
+    if(peopleName === 'Gosho'){
+        goshoServices.editGoshoFeedback(id, feedback)
+        
+    }
 }
