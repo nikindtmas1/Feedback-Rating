43539de0cfea314ff5c206fe87d8473707359831
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-04 12:17:27 +0200

    Create router put

--- server/src/controllers/peshoFeedbackController.js
+++ server/src/controllers/peshoFeedbackController.js
@@ -15,6 +15,11 @@ router.post('/', async (req, res) => {
     res.json({ ok: true });
 });
 
+router.put('/:id', async (req, res) => {
+    await services.updatePeshoFeedback(req.params.id, req.body);
+    res.json({ ok: true });
+});
+
 router.delete('/:id', async (req, res) => {
     await services.deletePeshoFeedback(req.params.id);
 
