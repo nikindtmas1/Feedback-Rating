0eed8895fdf2add2896a8c514ac34bef64509948
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-04 12:17:04 +0200

    Added function updatePeshoFeedback

--- server/src/services/peshoFeedbackService.js
+++ server/src/services/peshoFeedbackService.js
@@ -2,10 +2,12 @@ const PeshoFeedback = require('../models/peshoFeedbackModel');
 
 const getAllPeshoFeedback = () => PeshoFeedback.find();
 const createPeshoFeedback = (data) => PeshoFeedback.create(data);
+const updatePeshoFeedback = (id, data) => PeshoFeedback.findByIdAndUpdate(id, data);
 const deletePeshoFeedback = (id) => PeshoFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllPeshoFeedback,
     createPeshoFeedback,
+    updatePeshoFeedback,
     deletePeshoFeedback,
 };
