a5dcd387a5f0cdbb04b68b56ea0c8446735989d0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-05 10:18:20 +0200

    Create edit for Pesho data

--- client/src/Components/fetchFeedback/editFeedbackById.js
+++ client/src/Components/fetchFeedback/editFeedbackById.js
@@ -1,5 +1,7 @@
 import * as goshoServices from '../services/goshoData';
 import * as toshoServices from '../services/toshoData';
+import * as peshoServices from '../services/peshoData';
+
 
 export function editFeedbackById(id, peopleName, data) {
     if(peopleName === 'Gosho'){
@@ -10,5 +12,9 @@ export function editFeedbackById(id, peopleName, data) {
       return toshoServices
       .editToshoFeedback(id, data)
       .then(() => toshoServices.getAll());
+    }else if(peopleName === 'Pesho'){
+      return peshoServices
+      .editPeshoFeedback(id, data)
+      .then(() => peshoServices.getAll());
     }
 }
