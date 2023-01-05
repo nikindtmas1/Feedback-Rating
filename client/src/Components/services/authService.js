6ab6a593201ab33d36fcbc0c43b0b57e590096a9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-05 11:57:06 +0200

    Added refreshToken

--- client/src/Components/services/authService.js
+++ client/src/Components/services/authService.js
@@ -10,15 +10,18 @@ export const userInfoDate = {
   isAuthenticated: false,
   username: "",
   authToken: "",
+  refreshToken: "",
 };
 
 export const logInIvent = (logDate) => {
   let user = logDate.username;
   let authToken = logDate.accessToken;
+  let refreshToken = logDate.refreshToken;
 
   let userDate = {
     user: user,
     authToken: authToken,
+    refreshToken: refreshToken,
     isAuthenticated: true,
   };
 
