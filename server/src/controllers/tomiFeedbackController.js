b9d64f54f80b92e137316b4eeac8555cfde7284f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-09 10:01:11 +0200

    Formated and added router put for tomi feedback

--- server/src/controllers/tomiFeedbackController.js
+++ server/src/controllers/tomiFeedbackController.js
@@ -1,24 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const services = require('../services/tomiFeedbackService');
+const services = require("../services/tomiFeedbackService");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const tomiFeedbacks = await services.getAllTomiFeedback();
 
   res.json(tomiFeedbacks);
 });
 
-router.post('/', async (req, res) => {
-    await services.createTomiFeedback({...req.body});
+router.post("/", async (req, res) => {
+  await services.createTomiFeedback({ ...req.body });
 
-    res.json({ ok: true });
+  res.json({ ok: true });
 });
 
-router.delete('/:id', async (req, res) => {
-    await services.deleteTomiFeedback(req.params.id);
+router.put("/:id", async (req, res) => {
+  await services.updateTomiFeedback(req.params.id, req.body);
+  res.json({ ok: true });
+});
 
-    res.json({ ok: true });
+router.delete("/:id", async (req, res) => {
+  await services.deleteTomiFeedback(req.params.id);
+
+  res.json({ ok: true });
 });
 
 module.exports = router;
