48ec380fccb55b2d018be75692bbbae59d5f1a9d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-11 14:29:44 +0200

    Formmated user service

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,54 +1,50 @@
-const User = require('../models/userLoginModel');
+const User = require("../models/userLoginModel");
 
-const { createAccessToken, createRefreshToken } = require('../utils/jwtUtils');
-const bcriptUtils = require('../utils/bcriptUtils');
+const { createAccessToken, createRefreshToken } = require("../utils/jwtUtils");
+const bcriptUtils = require("../utils/bcriptUtils");
 
-exports.login = async ({username, password}) => {
-    const currUser = username;
-    const currPass = password;
+exports.login = async ({ username, password }) => {
+  const currUser = username;
+  const currPass = password;
 
-    const user = await User.findByUsername(currUser);
+  const user = await User.findByUsername(currUser);
 
-    if (!user) throw new Error("Invalid username!");
-    const valide = await bcriptUtils.verifyPass(currPass, user.password);
-    if (!valide) throw new Error("Invalid password!");
-    // if(user.password !== currPass) throw new Error("Invalid password!");
+  if (!user) throw new Error("Invalid username!");
+  const valide = await bcriptUtils.verifyPass(currPass, user.password);
+  if (!valide) throw new Error("Invalid password!");
+  // if(user.password !== currPass) throw new Error("Invalid password!");
 
-    if(user.username === currUser){
-        const accessToken = await createAccessToken(user);
-        const refreshToken = await createRefreshToken(user);
+  if (user.username === currUser) {
+    const accessToken = await createAccessToken(user);
+    const refreshToken = await createRefreshToken(user);
 
-          user.accessToken = accessToken;
-          user.refreshToken = refreshToken;
-          await user.save();
-      
-          return {user, accessToken, refreshToken};
-    };
+    user.accessToken = accessToken;
+    user.refreshToken = refreshToken;
+    await user.save();
 
+    return { user, accessToken, refreshToken };
+  }
 };
 
-exports.register = async ({username, password}) => {
-    const currUser = username;
-    const currPass = password;
+exports.register = async ({ username, password }) => {
+  const currUser = username;
+  const currPass = password;
 
-    const user = await User.findByUsername(currUser);
+  const user = await User.findByUsername(currUser);
 
-    if(!user){
-     
-            const hashPassword = await bcriptUtils.genHashPassword(currPass);
-            const username = currUser;
-            const password = hashPassword
-            const user = new User({username, password});
-            const accessToken = await createAccessToken(user);
-            const refreshToken = await createRefreshToken(user);
+  if (!user) {
+    const hashPassword = await bcriptUtils.genHashPassword(currPass);
+    const username = currUser;
+    const password = hashPassword;
+    const user = new User({ username, password });
+    const accessToken = await createAccessToken(user);
+    const refreshToken = await createRefreshToken(user);
 
-          user.accessToken = accessToken;
-          user.refreshToken = refreshToken;
-           await user.save();
-            return {user, accessToken, refreshToken};
-       
-    }else{
-        throw new Error('User name already exists!')
-    }
-
+    user.accessToken = accessToken;
+    user.refreshToken = refreshToken;
+    await user.save();
+    return { user, accessToken, refreshToken };
+  } else {
+    throw new Error("User name already exists!");
+  }
 };
