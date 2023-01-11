85a479723b51c113b817bae4be066d2574c10888
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-11 14:24:48 +0200

    Formated user controller

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,42 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const service = require('../services/userService');
-const { isGuest } = require('../middleware/authMidd');
+const service = require("../services/userService");
+const { isGuest } = require("../middleware/authMidd");
 
-router.post('/register', async (req, res) => {
-    const {username, password} = req.body;
-    try {
-        const {user, accessToken, refreshToken} = await service.register({username, password});
-        res.json({
-            _id: user._id,
-        username: user.username,
-        accessToken,
-        refreshToken,
-        })
-        
-    } catch (error) {
-        
-    }
-
-   
-});
-
-router.post('/login',isGuest, async (req, res) => {
-    const {username, password} = req.body;
-
-   const {user, accessToken, refreshToken} = await service.login({username, password});
-
-    res.json({
-        _id: user._id,
-        username: user.username,
-        accessToken,
-        refreshToken,
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const { user, accessToken, refreshToken } = await service.register({
+      username,
+      password,
     });
+    res.json({
+      _id: user._id,
+      username: user.username,
+      accessToken,
+      refreshToken,
+    });
+  } catch (error) {}
 });
 
-router.get('/logout', (req, res) => {
-    res.json({ ok: true });
+router.post("/login", isGuest, async (req, res) => {
+  const { username, password } = req.body;
+
+  const { user, accessToken, refreshToken } = await service.login({
+    username,
+    password,
   });
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    accessToken,
+    refreshToken,
+  });
+});
+
+router.get("/logout", (req, res) => {
+  res.json({ ok: true });
+});
 
 module.exports = router;
