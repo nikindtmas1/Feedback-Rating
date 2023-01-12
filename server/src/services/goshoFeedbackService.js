2081e83e5011e326acebbfdedb2dd9d206a04b1c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-12 12:12:19 +0200

    Added get one feedback

--- server/src/services/goshoFeedbackService.js
+++ server/src/services/goshoFeedbackService.js
@@ -1,12 +1,14 @@
 const GoshoFeedback = require('../models/goshoFeedbackModel');
 
 const getAllGoshoFeedback = () => GoshoFeedback.find();
+const getOneFeedback = (id) => GoshoFeedback.findById(id);
 const createGoshoFeedback = (data) => GoshoFeedback.create(data);
 const updateGoshoFeedback = (id, data) => GoshoFeedback.findByIdAndUpdate(id, data);
 const deleteGoshoFeedback = (id) => GoshoFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllGoshoFeedback,
+    getOneFeedback,
     createGoshoFeedback,
     updateGoshoFeedback,
     deleteGoshoFeedback,
