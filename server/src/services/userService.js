654d78500fba6da40da9ed092d672d38c386fa1f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-13 09:56:21 +0200

    Added function refresh and deactiveted

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -41,3 +41,19 @@ exports.register = async ({ username, password }) => {
    
   }
 };
+
+
+// exports.refresh = async (refreshToken) => {
+//   let { _id } = jwt.verify(refreshToken, 'MOGYSHTSECRET2');
+
+//   let user = await User.find({ _id, refreshToken });
+
+//   if (user) {
+//       let accessToken = jwt.sign({ _id: user._id, username: user.username }, 'MOGYSHTSECRET', { expiresIn: '1m' });
+//       let refreshToken = jwt.sign({ _id: user._id }, 'MOGYSHTSECRET2', { expiresIn: '7d' });
+
+//       return { accessToken, refreshToken };
+//   } else {
+//       return null;
+//   }
+// }
