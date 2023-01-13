bd00f24f67b0a5335dc87642e0b460d15471cb17
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-13 09:56:53 +0200

    Create function auth and deactiveted

--- server/src/middleware/authMidd.js
+++ server/src/middleware/authMidd.js
@@ -1,4 +1,27 @@
+const jwt = require('jsonwebtoken');
+// const { development } = require('../config/config');
+
+// const secretAccess = development.secretStr;
+
+// function auth(req, res, next) {
+//     // const token = req.headers['X-Authorization'];
+
+//     if(token){
+//         const decodetToken = jwt.verify(token, secretAccess)
+//         if(decodetToken){
+//             req.user = decodetToken;
+//             res.status(300)
+//             next();
+//         }else{
+//             res.status(401)
+//         }
+//     }else{
+//         next();
+//     }
+// }
+
 function isAuth(req, res, next) {
+    // console.log(req.headers["Content-Type"]);
     if(!req.user){
         return res.status(401)
     };
@@ -14,6 +37,7 @@ function isGuest(req, res, next) {
 };
 
 module.exports = {
+
     isAuth,
     isGuest,
 };
