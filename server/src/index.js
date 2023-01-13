664f2b87c9780bad1870b5fdb6974a124a481e37
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-13 11:27:41 +0200

    Import auth and start use

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 const app = express();
 
 const { development } = require("./config/config");
-// const {auth} = require('./middleware/authMidd');
+const {auth} = require('./middleware/authMidd');
 const port = development.PORT;
 
 const mongooseConfig = require("./config/configMongoose");
 const routes = require('./routes/routes');
 
 app.use(express.json());
-// app.use(auth);
+app.use(auth);
 app.use(cors());
 app.use(routes);
 mongooseConfig(app);
