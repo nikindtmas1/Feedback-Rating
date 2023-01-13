86f3d689e8467c329c08f01673e279330824b361
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-13 09:57:17 +0200

    Import middware and deactiveted

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(express.json());
 
 const { development } = require("./config/config");
+// const {auth} = require('./middleware/authMidd');
 const port = development.PORT;
 
 const mongooseConfig = require("./config/configMongoose");
 const routes = require('./routes/routes');
 
+app.use(express.json());
+// app.use(auth);
 app.use(cors());
 app.use(routes);
 mongooseConfig(app);
