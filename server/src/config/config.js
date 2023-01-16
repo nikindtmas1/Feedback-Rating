e33e99e9299e3627fdf6f8b13fe2f3f1b7fbb5a9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-16 10:50:19 +0200

    Change secrets

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     PORT: 5000,
     dbAtlas:
       "mongodb+srv://niki:niki548444@cluster0.8me3yuw.mongodb.net/feedback_datas?retryWrites=true&w=majority",
-    secretStr: "mnogoqkaparola",
-    secretStrTwo: "mymnogoqkaparolatwo",
+    secretStr: "mnogoqkaparolakokoboko",
+    secretStrTwo: "mymnogoqkaparolatwokokoboko",
   },
 
   production: {
