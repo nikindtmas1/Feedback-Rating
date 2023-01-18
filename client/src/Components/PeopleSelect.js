182248fcc42a35a95e6a2d69aff114e88984fd22
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-18 14:32:22 +0200

    Formated

--- client/src/Components/PeopleSelect.js
+++ client/src/Components/PeopleSelect.js
@@ -1,36 +1,44 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { FeedbackContext } from "../context/FeedbackContext";
 
-
-
 const SelectPeople = () => {
+  const { feedbackByName } = useContext(FeedbackContext);
 
-    const {feedbackByName} = useContext(FeedbackContext);
+  const onOptionChangeHandler = (e) => {
+    let currentName = e.target.value;
+    feedbackByName(currentName);
+  };
 
-    
-
-    const onOptionChangeHandler = (e) => {
-   
-        let currentName = e.target.value;
-        feedbackByName(currentName);
-      
-    }
-
-
-
-    return (
-        <>
-        <select onChange={onOptionChangeHandler} style={{width: 140, color: 'blue', borderRadius: "10px", padding: "3px"}}>
-            <option value='start' style={{textAlign: "center"}}>Select Therapist</option>
-            <option value='Gosho' style={{textAlign: "center"}}>Gosho</option>
-            <option value='Tosho' style={{textAlign: "center"}}>Tosho</option>
-            <option value='Pesho' style={{textAlign: "center"}}>Pesho</option>
-            <option value='Tomi' style={{textAlign: "center"}}>Tomi</option>
-            
-        </select>
-        {/* <button style={{borderRadius: '5px'}} type="button" onClick={onClick}>Submit</button> */}
-        </>
-    );
+  return (
+    <>
+      <select
+        onChange={onOptionChangeHandler}
+        style={{
+          width: 140,
+          color: "blue",
+          borderRadius: "10px",
+          padding: "3px",
+        }}
+      >
+        <option value="start" style={{ textAlign: "center" }}>
+          Select Therapist
+        </option>
+        <option value="Gosho" style={{ textAlign: "center" }}>
+          Gosho
+        </option>
+        <option value="Tosho" style={{ textAlign: "center" }}>
+          Tosho
+        </option>
+        <option value="Pesho" style={{ textAlign: "center" }}>
+          Pesho
+        </option>
+        <option value="Tomi" style={{ textAlign: "center" }}>
+          Tomi
+        </option>
+      </select>
+      {/* <button style={{borderRadius: '5px'}} type="button" onClick={onClick}>Submit</button> */}
+    </>
+  );
 };
 
 export default SelectPeople;
