cd110cc48778cca132823a3d908059c0210bc9bc
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-18 13:58:36 +0200

    Change ternary operators

--- client/src/Components/fetchFeedback/fetchFeedbacks.js
+++ client/src/Components/fetchFeedback/fetchFeedbacks.js
@@ -4,16 +4,10 @@ import * as toshoService from "../services/toshoData";
 import * as peshoService from "../services/peshoData";
 import * as tomiService from '../services/tomiData';
 
-export function fetchByName(name) {
-  if (name === "Gosho") {
-    return goshoService.getAll();
-  } else if (name === "Tosho") {
-    return toshoService.getAll();
-  } else if (name === "Pesho") {
-    return peshoService.getAll();
-  } else if (name === "Tomi") {
-    return tomiService.getAll();
-  } else if (name === "start") {
-    return service.getAll();
-  }
+export function fetchByName(name){
+  return name === 'Gosho' ? goshoService.getAll()
+  : name === 'Tosho' ? toshoService.getAll()
+  : name === 'Pesho' ? peshoService.getAll()
+  : name === 'Tomi' ? tomiService.getAll()
+  :  service.getAll()
 }
