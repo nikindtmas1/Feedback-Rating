acc614644529506f07489669b8eb5fccec6684ba
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-18 14:34:19 +0200

    Formated

--- client/src/Components/AboutIconLink.js
+++ client/src/Components/AboutIconLink.js
@@ -1,17 +1,14 @@
-// import {FaQuestion} from 'react-icons/fa';
-// import { FaSistrix } from 'react-icons/fa';
-import { FaLightbulb } from 'react-icons/fa';
-import  {Link} from 'react-router-dom';
+import { FaLightbulb } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const AboutIconLink = () => {
-    return (
-        <div className='about-link'>
-            <Link to = {'/login'}>
-                {/* <FaQuestion size={30} />   */}
-                       <FaLightbulb size={30} />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="about-link">
+      <Link to={"/login"}>
+        <FaLightbulb size={30} />
+      </Link>
+    </div>
+  );
 };
 
 export default AboutIconLink;
