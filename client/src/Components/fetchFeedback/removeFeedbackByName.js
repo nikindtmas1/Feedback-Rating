f1857759dae47321206be45434a2904b7d2c22dc
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-18 14:10:41 +0200

    Added ternary operators

--- client/src/Components/fetchFeedback/removeFeedbackByName.js
+++ client/src/Components/fetchFeedback/removeFeedbackByName.js
@@ -3,20 +3,11 @@ import * as toshoService from '../services/toshoData';
 import * as peshoService from '../services/peshoData';
 import * as tomiService from '../services/tomiData';
 
-
-export function deleteFeedbackByName(id, peopleName, feedback) {
-   
-   if(peopleName === 'Gosho'){
-    return goshoService.deleteGoshoFeedback(id)
-    .then(() => feedback.filter((item) => item._id !== id))
-   }else if(peopleName === 'Tosho'){
-    return toshoService.deleteToshoFeedback(id)
-    .then(() => feedback.filter((item) => item._id !== id))
-   }else if(peopleName === 'Pesho'){
-    return peshoService.deletePeshoFeedback(id)
-    .then(() => feedback.filter((item) => item._id !== id))
-   }else if(peopleName === 'Tomi'){
-    return tomiService.deleteTomiFeedback(id)
-    .then(() => feedback.filter((item) => item._id !== id))
-   }
+export function deleteFeedbackByName(id, peopleName, feedback){
+   return peopleName === 'Gosho' ?
+   goshoService.deleteGoshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : peopleName === 'Tosho' ? toshoService.deleteToshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : peopleName === 'Pesho' ? peshoService.deletePeshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : peopleName === 'Tomi' ? tomiService.deleteTomiFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : null
 }
