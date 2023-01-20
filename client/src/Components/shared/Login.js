075c3d2c5bdf85764b594dd258e3bb52abfc0e66
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-20 14:03:31 +0200

    Update Login.js

--- client/src/Components/shared/Login.js
+++ client/src/Components/shared/Login.js
@@ -1,7 +1,30 @@
-import React from "react";
-import Button from "./Button";
+import React, {useState, useEffect} from "react";
+import Button from "./ButtonLogin";
 
 const Login = () => {
+
+const [btnDisabled, setBtnDisabled] = useState(true);
+const [loginUserName, setLoginUserName] = useState('');
+const [userPass, setUserPass] = useState('');
+
+useEffect(() => {
+
+  if(loginUserName.length === 0 || userPass.length === 0){
+    setBtnDisabled(true);
+  }else{
+    setBtnDisabled(false)
+  };
+
+},[loginUserName, userPass]);
+
+const handleUserLoginChange = (e) => {
+  setLoginUserName(e.target.value);
+};
+
+const handleUserPass = (e) => {
+  setUserPass(e.target.value)
+};
+
   return (
     <>
       <h2
@@ -17,7 +40,10 @@ const Login = () => {
         placeholder="User Name" 
         pattern="[a-z]+" 
         title="Only lowercase letters allowed!" 
-        required />
+        required
+        value={loginUserName}
+        onChange={handleUserLoginChange}
+        />
       </div>
       <div className="input-group">
         <input type="password" 
@@ -26,10 +52,13 @@ const Login = () => {
         pattern="[a-z0-9]+"
         title="Password is not correct!"
         size='20'
-        required />
+        required 
+        value={userPass}
+        onChange={handleUserPass}
+        />
       </div>
       <div className="button-login">
-        <Button type="submit" disabled="false" version="tertiary">
+        <Button type="submit" isDisabled={btnDisabled} version="tertiary">
           Submit
         </Button>
       </div>
