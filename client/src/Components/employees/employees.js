ca19c21f5f963321594ab2723889341c8684964d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-23 11:13:03 +0200

    Create employees

---
+++ client/src/Components/employees/employees.js
@@ -0,0 +1,7 @@
+export const therapiests = [
+    {'name': 'start', 'title': 'Select Therapist'},
+    {'name': 'Gosho', 'title': 'Gosho'}, 
+    {'name': 'Tosho', 'title': 'Tosho'},
+    {'name': 'Pesho', 'title': 'Pesho'}, 
+    {'name': 'Tomi', 'title': 'Tomi'},
+];
