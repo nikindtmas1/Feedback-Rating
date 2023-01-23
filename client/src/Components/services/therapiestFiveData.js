a180f7d0eedae1ceeec1fe497d3b2625624a3d52
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-23 14:36:24 +0200

    Create function for five therapiest

---
+++ client/src/Components/services/therapiestFiveData.js
@@ -0,0 +1,15 @@
+import * as services from '../services/feedbackService';
+
+const host = 'http://localhost:5000';
+
+services.settings.host = host;
+
+export async function getAll() {
+  const therFive =  await services.get(host + '/therfivefeedbacks');
+  const result = Object.values(therFive);
+  return result;
+};
+
+export async function createTherFiveFeedback(data) {
+   return await services.post(host + '/therfivefeedbacks', data);
+};
