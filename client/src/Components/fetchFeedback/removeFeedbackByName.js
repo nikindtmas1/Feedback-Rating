de43963ab4f0c72288bb6ce174b9b4c178e61ff0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-24 10:31:40 +0200

    Added therFive delete function

--- client/src/Components/fetchFeedback/removeFeedbackByName.js
+++ client/src/Components/fetchFeedback/removeFeedbackByName.js
@@ -2,6 +2,7 @@ import * as goshoService from '../services/goshoData';
 import * as toshoService from '../services/toshoData';
 import * as peshoService from '../services/peshoData';
 import * as tomiService from '../services/tomiData';
+import * as therFiveService from '../services/therapiestFiveData';
 
 export function deleteFeedbackByName(id, peopleName, feedback){
    return peopleName === 'Gosho' ?
@@ -9,5 +10,6 @@ export function deleteFeedbackByName(id, peopleName, feedback){
    : peopleName === 'Tosho' ? toshoService.deleteToshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : peopleName === 'Pesho' ? peshoService.deletePeshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : peopleName === 'Tomi' ? tomiService.deleteTomiFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : peopleName === 'Svetla' ? therFiveService.deleteTherFeveFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : null
 }
