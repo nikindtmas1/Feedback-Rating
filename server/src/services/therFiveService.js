4d32fc62003cc52e35e54f60ef45286c3b580f17
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-24 10:30:35 +0200

    Added delete function

--- server/src/services/therFiveService.js
+++ server/src/services/therFiveService.js
@@ -2,8 +2,10 @@ const TherFiveFeedback = require('../models/therFiveModel');
 
 const getAllTherFiveFeedbacks = () => TherFiveFeedback.find();
 const createTherFiveFeedback = (data) => TherFiveFeedback.create(data);
+const deleteTherFiveFeedback = (id) => TherFiveFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllTherFiveFeedbacks,
     createTherFiveFeedback,
+    deleteTherFiveFeedback,
 }
