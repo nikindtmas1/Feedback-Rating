5ab837c9e23bbaf7dd3203af9fa05a4d3d640e89
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-25 11:22:15 +0200

    Added create employee

--- server/src/services/employeeService.js
+++ server/src/services/employeeService.js
@@ -1,7 +1,9 @@
 const Employee = require('../models/employeesModel');
 
 const getAllEmployees = () => Employee.find();
+const createEmployee = (data) => Employee.create(data);
 
 module.exports = {
     getAllEmployees,
-}
+    createEmployee,
+};
