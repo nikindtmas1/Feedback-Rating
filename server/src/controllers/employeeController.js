5de5011e7a5c3640dbd6e0f9704dbdf9b3ac6ee4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-25 11:33:32 +0200

    Create get all employees

--- server/src/controllers/employeeController.js
+++ server/src/controllers/employeeController.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const service = require('../services/employeeService');
 
+router.get('/', async (req, res) => {
+    const employees = await service.getAllEmployees();
+    res.json(employees);
+});
 
 module.exports = router;
