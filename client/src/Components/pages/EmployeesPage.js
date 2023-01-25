0c5457186cac704a876afd11353e00088bde8ce5
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-25 14:10:02 +0200

    Added form

--- client/src/Components/pages/EmployeesPage.js
+++ client/src/Components/pages/EmployeesPage.js
@@ -1,25 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../shared/Button";
+import Card from "../shared/Card";
 
-import * as employeeService from '../services/employeeData';
+import * as employeeService from "../services/employeeData";
 
 const EmployeesPage = () => {
+  const [employee, setEmployee] = useState([]);
+
   useEffect(() => {
-    employeeService.getAll()
-    .then((result) =>  console.log(result))
-   
+    employeeService.getAll().then((result) => setEmployee(result));
   }, []);
 
   return (
-    <div>
-      <h3>EmployeesPage</h3>
+    <div className="app-body text-login">
+      <Card>
+        <div className="text-container">
+          <div className="text-content">
+            <h3
+              style={{
+                color: "rgb(45, 179, 206)",
+              }}
+            >
+              Change Therapiests
+            </h3>
+            <form>
+              <label for="fname">{" "}Number{" "}:{" "}</label>
+          
+              <input type="text" id="fname" name="fname" value={employee[0].title}/>
+            
+              
+              <div className="input-group">
+              <label for="lname">First name:</label>
 
-      <Button version="secondary">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          Back
-        </Link>
-      </Button>
+              <input type="text" id="lname" name="lname" />
+              </div>
+            </form>
+            {/* <form>
+              
+            <div className="input-group">
+            <input type='text'>Name</input>
+            </div>
+            <div className="input-group">
+            <input type='text'>Title</input>
+
+            </div>
+          </form> */}
+            <Button version="secondary">
+              <Link to="/" style={{ textDecoration: "none" }}>
+                Back
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </Card>
     </div>
   );
 };
