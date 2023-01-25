123876b38930dffff0b041db7c58012b8a4569d5
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-25 11:12:34 +0200

    Import employees page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Header from "./Components/Header";
 import LoginPage from "./Components/pages/LoginPage";
 import Dashboard from "./Components/pages/Dashboard";
 import Logout from "./Components/pages/Logout";
+import EmployeesPage from "./Components/pages/EmployeesPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" exact component={Dashboard} />
           <Route path="/login" component={LoginPage} />
           <Route path="/logout" component={Logout} />
+          <Route path="/employees" component={EmployeesPage} />
         </body>
       </div>
     </FeedbackProvider>
