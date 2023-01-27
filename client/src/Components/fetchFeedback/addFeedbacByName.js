921c89c487029ef335879b38aa00bb88325d2c82
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-27 11:19:50 +0200

    Added properti

--- client/src/Components/fetchFeedback/addFeedbacByName.js
+++ client/src/Components/fetchFeedback/addFeedbacByName.js
@@ -4,8 +4,9 @@ import * as peshoService from "../services/peshoData";
 import * as tomiService from '../services/tomiData';
 import * as therFiveService from '../services/therapiestFiveData';
 
-export function addFeedbackByName(data){
-  const name = data.peopleName;
+export function addFeedbackByName(data, therName){
+
+  const name = therName;
   return name === 'Gosho' ? 
   goshoService.createGoshoFeedback(data).then(() => goshoService.getAll())
   : name === 'Tosho' ? toshoService.createToshoFeedback(data).then(() => toshoService.getAll())
