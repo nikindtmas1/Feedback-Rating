4c302001ed4af9b5a7e4c96d3409f4aaae4b5d47
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-27 11:20:03 +0200

    Change therName

--- client/src/Components/fetchFeedback/editFeedbackById.js
+++ client/src/Components/fetchFeedback/editFeedbackById.js
@@ -4,12 +4,12 @@ import * as peshoServices from '../services/peshoData';
 import * as tomiServices from '../services/tomiData';
 import * as therFiveService from '../services/therapiestFiveData';
 
-export function editFeedbackById(id, peopleName, data){
-  return peopleName === 'Gosho' ?
+export function editFeedbackById(id, therName, data){
+  return therName === 'Gosho' ?
   goshoServices.editGoshoFeedback(id, data).then(() => goshoServices.getAll())
-  : peopleName === 'Tosho' ? toshoServices.editToshoFeedback(id, data).then(() => toshoServices.getAll())
-  : peopleName === 'Pesho' ? peshoServices.editPeshoFeedback(id, data).then(() => peshoServices.getAll())
-  : peopleName === 'Tomi' ? tomiServices.editTomiFeedback(id, data).then(() => tomiServices.getAll())
-  : peopleName === 'Five' ? therFiveService.editTherFiveFeedback(id, data).then(() => therFiveService.getAll())
+  : therName === 'Tosho' ? toshoServices.editToshoFeedback(id, data).then(() => toshoServices.getAll())
+  : therName === 'Pesho' ? peshoServices.editPeshoFeedback(id, data).then(() => peshoServices.getAll())
+  : therName === 'Tomi' ? tomiServices.editTomiFeedback(id, data).then(() => tomiServices.getAll())
+  : therName === 'Five' ? therFiveService.editTherFiveFeedback(id, data).then(() => therFiveService.getAll())
   : null
 }
