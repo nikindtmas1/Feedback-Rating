7b0845daaec6238e8486d4bd856b01e09ceed313
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-27 11:42:55 +0200

    Change reference

--- client/src/Components/fetchFeedback/fetchFeedbacks.js
+++ client/src/Components/fetchFeedback/fetchFeedbacks.js
@@ -7,7 +7,7 @@ import * as therFiveService from '../services/therapiestFiveData';
 
 export function fetchByName(name){
   return name === 'Gosho' ? goshoService.getAll()
-  : name === 'Tosho' ? toshoService.getAll()
+  : name === 'Two' ? toshoService.getAll()
   : name === 'Three' ? peshoService.getAll()
   : name === 'Four' ? tomiService.getAll()
   : name === 'Five' ? therFiveService.getAll()
