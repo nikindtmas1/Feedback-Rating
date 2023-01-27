c79d0921632de9c3dfb5fc38967ecfb16426d462
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-27 14:31:52 +0200

    Added function edit employee

--- client/src/Components/services/employeeData.js
+++ client/src/Components/services/employeeData.js
@@ -9,3 +9,12 @@ export async function getAll() {
  const result = Object.values(employees);
  return result; 
 };
+
+export async function createEmplyees(data) {
+    return await services.post(host + `/employees`, data);
+   
+};
+
+export async function editEmployee(id, data) {
+    return await services.put(host + `/employees/${id}`, data);
+};
