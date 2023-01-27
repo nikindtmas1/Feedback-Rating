91328cbc42e7aea4e0f674de78dda01e91524bf3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-27 11:38:40 +0200

    Change reference

--- client/src/Components/fetchFeedback/removeFeedbackByName.js
+++ client/src/Components/fetchFeedback/removeFeedbackByName.js
@@ -8,7 +8,7 @@ export function deleteFeedbackByName(id, therName, feedback){
    return therName === 'Gosho' ?
    goshoService.deleteGoshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : therName === 'Tosho' ? toshoService.deleteToshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
-   : therName === 'Pesho' ? peshoService.deletePeshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : therName === 'Three' ? peshoService.deletePeshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : therName === 'Four' ? tomiService.deleteTomiFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : therName === 'Five' ? therFiveService.deleteTherFeveFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : null
