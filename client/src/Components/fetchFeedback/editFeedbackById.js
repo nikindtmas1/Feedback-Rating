031f41398af080206317c4ba011b280875f0563a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-27 11:42:45 +0200

    Change reference

--- client/src/Components/fetchFeedback/editFeedbackById.js
+++ client/src/Components/fetchFeedback/editFeedbackById.js
@@ -7,7 +7,7 @@ import * as therFiveService from '../services/therapiestFiveData';
 export function editFeedbackById(id, therName, data){
   return therName === 'Gosho' ?
   goshoServices.editGoshoFeedback(id, data).then(() => goshoServices.getAll())
-  : therName === 'Tosho' ? toshoServices.editToshoFeedback(id, data).then(() => toshoServices.getAll())
+  : therName === 'Two' ? toshoServices.editToshoFeedback(id, data).then(() => toshoServices.getAll())
   : therName === 'Three' ? peshoServices.editPeshoFeedback(id, data).then(() => peshoServices.getAll())
   : therName === 'Four' ? tomiServices.editTomiFeedback(id, data).then(() => tomiServices.getAll())
   : therName === 'Five' ? therFiveService.editTherFiveFeedback(id, data).then(() => therFiveService.getAll())
