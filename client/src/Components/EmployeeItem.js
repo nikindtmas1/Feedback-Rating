0d549911233fd6097cb4bcf471eb80dc67ee3893
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-01-31 13:28:14 +0200

    Update employeeItem.js

--- client/src/Components/EmployeeItem.js
+++ client/src/Components/EmployeeItem.js
@@ -1,31 +1,37 @@
-import React, {useContext} from 'react';
-import { FeedbackContext } from '../context/FeedbackContext';
-import { FaEdit } from 'react-icons/fa';
-import Card from './shared/Card';
+import React, { useContext } from "react";
+import { FeedbackContext } from "../context/FeedbackContext";
+import { FaEdit } from "react-icons/fa";
+import Card from "./shared/Card";
 
-const EmployeeItem = ({item}) => {
-    const { isAuth, editEmployee } = useContext(FeedbackContext);
- 
+const EmployeeItem = ({ item }) => {
+  const { isAuth, editEmployee } = useContext(FeedbackContext);
 
   return (
     <Card>
-
-        {isAuth ? (
-            <div style={{width: '280px'}}>
-                <input style={{padding: '2px'}} type="checkbox" checked={item.check}  />
-                {/* <button type='button' className='close'>
+      {isAuth ? (
+        <div style={{ width: "280px" }}>
+          {/* <button type='button' className='close'>
                     <FaTimes color='red'/>
                 </button> */}
-                <button type='button' className='edit' onClick={() => editEmployee(item)}>
-                    <FaEdit color='purple'/>
-                </button>
-            </div>
-
-        ) : null
-    }
-    <div className='text-display'>{item.title}</div>
+          <button
+            type="button"
+            className="edit"
+            onClick={() => editEmployee(item)}
+          >
+            <FaEdit color="purple" />
+          </button>
+        </div>
+      ) : null}
+      <div className="text-display">
+        <input
+          style={{ padding: "2px" }}
+          type="checkbox"
+          checked={item.check}
+        />{" "}
+        {item.title}
+      </div>
     </Card>
   );
 };
 
-export default EmployeeItem
+export default EmployeeItem;
