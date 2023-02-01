313737890b45a6a7633eb6ad5c2cbd5694653091
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-01 13:48:19 +0200

    Update Alert.js

--- client/src/Components/shared/Alert.js
+++ client/src/Components/shared/Alert.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
+import style from '../style.module.css';
+import css from 'classnames';
 
-const Alert = () => {
+const Alert = ({ children, type, message }) => {
+
+    const [isShow, setIsShow] = useState(true);
+
+    const renderElAlert = function () {
+        return React.cloneElement(children);
+      };
+
+      const handleClose = (e) => {
+        e.preventDefault();
+        setIsShow(false);
+      };
+
   return (
-    <div>
-      <span> &times;</span>
-        message
-    </div>
+    <div className={css(style.alert, style[type], !isShow && style.hide)}>
+    <span className={style.closebtn} onClick={handleClose}>
+      &times;
+    </span>
+    {children ? renderElAlert() : message}
+  </div>
   )
 }
 
