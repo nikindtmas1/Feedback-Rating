37e9bbdc6c06690ad8d6a239248d694e74ff42aa
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-01 16:22:25 +0200

    Added isAuth

--- client/src/Components/AboutIconLink.js
+++ client/src/Components/AboutIconLink.js
@@ -1,13 +1,19 @@
 import { FaLightbulb } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { FeedbackContext } from "../context/FeedbackContext";
 
 const AboutIconLink = () => {
+  const {isAuth} = useContext(FeedbackContext);
+
   return (
+    isAuth === false ?
     <div className="about-link">
       <Link to={"/login"}>
         <FaLightbulb size={30} />
       </Link>
     </div>
+    : null
   );
 };
 
