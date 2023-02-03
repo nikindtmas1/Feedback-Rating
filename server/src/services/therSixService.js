d49f454f1aae4d03c2c7683702e1dd3c09cfec1d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-03 11:35:55 +0200

    Added delete feedback

--- server/src/services/therSixService.js
+++ server/src/services/therSixService.js
@@ -2,10 +2,13 @@ const TherSixFeedback = require('../models/therSixModel');
 
 const getAllTherSixFeedback = () => TherSixFeedback.find();
 const createTherSixFeedback = (data) => TherSixFeedback.create(data);
-
+const updateTherSixFeedback = (id, data) => TherSixFeedback.findByIdAndUpdate(id, data);
+const deleteTherSixFeedback = (id) => TherSixFeedback.findByIdAndRemove(id);
 
 
 module.exports = {
     getAllTherSixFeedback,
     createTherSixFeedback,
+    updateTherSixFeedback,
+    deleteTherSixFeedback
 };
