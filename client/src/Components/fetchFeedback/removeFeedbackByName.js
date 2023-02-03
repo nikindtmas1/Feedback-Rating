c8dd578777865d18d48c623daa58a967c5d039ca
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-03 11:05:26 +0200

    Added ther six

--- client/src/Components/fetchFeedback/removeFeedbackByName.js
+++ client/src/Components/fetchFeedback/removeFeedbackByName.js
@@ -3,6 +3,7 @@ import * as toshoService from '../services/toshoData';
 import * as peshoService from '../services/peshoData';
 import * as tomiService from '../services/tomiData';
 import * as therFiveService from '../services/therapiestFiveData';
+import * as therSixService from '../services/therSixData';
 
 export function deleteFeedbackByName(id, therName, feedback){
    return therName === 'One' ?
@@ -11,5 +12,6 @@ export function deleteFeedbackByName(id, therName, feedback){
    : therName === 'Three' ? peshoService.deletePeshoFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : therName === 'Four' ? tomiService.deleteTomiFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : therName === 'Five' ? therFiveService.deleteTherFeveFeedback(id).then(() => feedback.filter((item) => item._id !== id))
+   : therName === 'Six' ? therSixService.deleteTherSixFeedback(id).then(() => feedback.filter((item) => item._id !== id))
    : null
 }
