6b411551056da07c27cb682688c49f3bab1f8224
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-03 11:02:28 +0200

    Added thersix

--- client/src/Components/fetchFeedback/fetchFeedbacks.js
+++ client/src/Components/fetchFeedback/fetchFeedbacks.js
@@ -4,6 +4,7 @@ import * as toshoService from "../services/toshoData";
 import * as peshoService from "../services/peshoData";
 import * as tomiService from '../services/tomiData';
 import * as therFiveService from '../services/therapiestFiveData';
+import * as therSixService from '../services/therSixData';
 
 export function fetchByName(name){
   return name === 'One' ? goshoService.getAll()
@@ -11,5 +12,6 @@ export function fetchByName(name){
   : name === 'Three' ? peshoService.getAll()
   : name === 'Four' ? tomiService.getAll()
   : name === 'Five' ? therFiveService.getAll()
+  : name === 'Six' ? therSixService.getAll()
   :  service.getAll()
 }
