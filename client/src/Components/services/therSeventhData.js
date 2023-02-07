83e3e7bfd52a3d915060160321485b4da063c7bb
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-07 10:01:45 +0200

    Create therSeventhData.js

---
+++ client/src/Components/services/therSeventhData.js
@@ -0,0 +1,11 @@
+import * as service from '../services/feedbackService';
+
+const host = 'http://localhost:5000';
+
+service.settings.host = host;
+
+export async function getAll() {
+    const seventTherFeed = await service.get(host + '/therseventhfeedbacks');
+    const result = Object.values(seventTherFeed);
+    return result;
+};
