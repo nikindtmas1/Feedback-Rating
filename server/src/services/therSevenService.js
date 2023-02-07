99230d1a41f38806282cf3d9bfbb23dc7a882ce9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-07 10:34:16 +0200

    Added create function

--- server/src/services/therSevenService.js
+++ server/src/services/therSevenService.js
@@ -1,7 +1,9 @@
 const TherSevenFeedback = require('../models/therSevenModel');
 
 const getAllTherSevenFeedback = () => TherSevenFeedback.find();
+const createTherSevenFeedback = (data) => TherSevenFeedback.create(data);
 
 module.exports = {
     getAllTherSevenFeedback,
+    createTherSevenFeedback,
 }
