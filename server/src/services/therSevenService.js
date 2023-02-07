8563e91d1cf5aa03b4a64e98f6bd2539fbb4e076
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-07 10:35:22 +0200

    Added update function

--- server/src/services/therSevenService.js
+++ server/src/services/therSevenService.js
@@ -2,8 +2,10 @@ const TherSevenFeedback = require('../models/therSevenModel');
 
 const getAllTherSevenFeedback = () => TherSevenFeedback.find();
 const createTherSevenFeedback = (data) => TherSevenFeedback.create(data);
+const updateTherSevenFeedback = (id, data) => TherSevenFeedback.findByIdAndUpdate(id, data);
 
 module.exports = {
     getAllTherSevenFeedback,
     createTherSevenFeedback,
+    updateTherSevenFeedback,
 }
