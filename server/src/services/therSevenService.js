554ecede354b1de41b5a2e25c681aa6919e9bc64
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-07 10:36:30 +0200

    Added delete function

--- server/src/services/therSevenService.js
+++ server/src/services/therSevenService.js
@@ -3,9 +3,11 @@ const TherSevenFeedback = require('../models/therSevenModel');
 const getAllTherSevenFeedback = () => TherSevenFeedback.find();
 const createTherSevenFeedback = (data) => TherSevenFeedback.create(data);
 const updateTherSevenFeedback = (id, data) => TherSevenFeedback.findByIdAndUpdate(id, data);
+const deleteTherSevenFeedback = (id) => TherSevenFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllTherSevenFeedback,
     createTherSevenFeedback,
     updateTherSevenFeedback,
-}
+    deleteTherSevenFeedback,
+};
