bdad922e7cb940d233108d1fb1e10ba8bc8330d0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-07 10:23:05 +0200

    Added get All ther Seven

--- client/src/Components/fetchFeedback/fetchFeedbacks.js
+++ client/src/Components/fetchFeedback/fetchFeedbacks.js
@@ -5,6 +5,7 @@ import * as peshoService from "../services/peshoData";
 import * as tomiService from '../services/tomiData';
 import * as therFiveService from '../services/therapiestFiveData';
 import * as therSixService from '../services/therSixData';
+import * as therSevService from '../services/therSeventhData';
 
 export function fetchByName(name){
   return name === 'One' ? goshoService.getAll()
@@ -13,5 +14,6 @@ export function fetchByName(name){
   : name === 'Four' ? tomiService.getAll()
   : name === 'Five' ? therFiveService.getAll()
   : name === 'Six' ? therSixService.getAll()
+  : name === 'Seven' ? therSevService.getAll()
   :  service.getAll()
 }
