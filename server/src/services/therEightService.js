ff02267ae262e05bb70e3abde1464e07b6e2ac26
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-09 11:36:24 +0200

    Added update therEight feddback

--- server/src/services/therEightService.js
+++ server/src/services/therEightService.js
@@ -2,9 +2,10 @@ const TherEightFeedback = require('../models/therEightModel');
 
 const getAllTherEightFeedbacks = () => TherEightFeedback.find();
 const createTherEightFeedback = (data) => TherEightFeedback.create(data);
-
+const updateTherEightFeedback = (id, data) => TherEightFeedback.findByIdAndUpdate(id, data);
 
 module.exports = {
     getAllTherEightFeedbacks,
     createTherEightFeedback,
+    updateTherEightFeedback,
 };
