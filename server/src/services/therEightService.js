e49022c040714eddbc65460e575829e1c815c3df
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-09 11:37:36 +0200

    Added delete therEight feedback

--- server/src/services/therEightService.js
+++ server/src/services/therEightService.js
@@ -3,9 +3,11 @@ const TherEightFeedback = require('../models/therEightModel');
 const getAllTherEightFeedbacks = () => TherEightFeedback.find();
 const createTherEightFeedback = (data) => TherEightFeedback.create(data);
 const updateTherEightFeedback = (id, data) => TherEightFeedback.findByIdAndUpdate(id, data);
+const deleteTherEightFeedback = (id) => TherEightFeedback.findByIdAndDelete(id);
 
 module.exports = {
     getAllTherEightFeedbacks,
     createTherEightFeedback,
     updateTherEightFeedback,
+    deleteTherEightFeedback,
 };
