af0ea13ceadf7a30470b30244f878b8cd1d56b82
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-09 11:32:14 +0200

    Added create therEight feedback

--- server/src/services/therEightService.js
+++ server/src/services/therEightService.js
@@ -1,3 +1,10 @@
 const TherEightFeedback = require('../models/therEightModel');
 
 const getAllTherEightFeedbacks = () => TherEightFeedback.find();
+const createTherEightFeedback = (data) => TherEightFeedback.create(data);
+
+
+module.exports = {
+    getAllTherEightFeedbacks,
+    createTherEightFeedback,
+};
