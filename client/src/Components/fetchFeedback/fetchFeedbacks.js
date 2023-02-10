8e2e2ead88c6f735cf4e98855c8036391dc6d646
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-10 13:43:19 +0200

    Added getAll feedbacks

--- client/src/Components/fetchFeedback/fetchFeedbacks.js
+++ client/src/Components/fetchFeedback/fetchFeedbacks.js
@@ -7,6 +7,7 @@ import * as therFiveService from '../services/therapiestFiveData';
 import * as therSixService from '../services/therSixData';
 import * as therSevService from '../services/therSeventhData';
 import * as therEightService from '../services/therEightData';
+import * as therNineService from '../services/therNineData';
 
 export function fetchByName(name){
   return name === 'One' ? goshoService.getAll()
@@ -17,5 +18,6 @@ export function fetchByName(name){
   : name === 'Six' ? therSixService.getAll()
   : name === 'Seven' ? therSevService.getAll()
   : name === 'Eight' ? therEightService.getAll()
+  : name === 'Nine' ? therNineService.getAll()
   :  service.getAll()
 };
