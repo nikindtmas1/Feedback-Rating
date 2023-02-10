ea26b31448de6cbcd83c9bd93b2120dc86354e57
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-10 14:02:55 +0200

    Added createTherNineFeedback

--- server/src/services/therNineService.js
+++ server/src/services/therNineService.js
@@ -1,8 +1,10 @@
 const TherNineFeedback = require('../models/therNineModel');
 
 const getAllTherNienFeedbacks = () => TherNineFeedback.find();
+const createTherNineFeedback = (data) => TherNineFeedback.create(data);
 
 
 module.exports = {
     getAllTherNienFeedbacks,
+    createTherNineFeedback,
 };
