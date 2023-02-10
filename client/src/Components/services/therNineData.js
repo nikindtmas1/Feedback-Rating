9e743b587ee31b5bcbf89bd3f0ee5472cce5a2dd
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-10 12:50:35 +0200

    Create therNineData.js

---
+++ client/src/Components/services/therNineData.js
@@ -0,0 +1,13 @@
+import * as services from '../services/feedbackService';
+
+const host = 'http://localhost:5000';
+
+
+services.settings.host = host;
+
+
+export async function getAll() {
+    const therNineFeedbacks = await services.get(host + '/therninefeedbacks');
+    const result = Object.values(therNineFeedbacks);
+    return result;
+};
