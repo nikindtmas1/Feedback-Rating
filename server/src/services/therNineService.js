2d576269d96994c1d6b3f1684a51468854194d14
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-10 14:05:17 +0200

    Added delete therNineFeedback

--- server/src/services/therNineService.js
+++ server/src/services/therNineService.js
@@ -3,10 +3,11 @@ const TherNineFeedback = require('../models/therNineModel');
 const getAllTherNienFeedbacks = () => TherNineFeedback.find();
 const createTherNineFeedback = (data) => TherNineFeedback.create(data);
 const updateTherNineFeedback = (id, data) => TherNineFeedback.findByIdAndUpdate(id, data);
-
+const deleteTherNineFeedback = (id) => TherNineFeedback.findByIdAndRemove(id);
 
 module.exports = {
     getAllTherNienFeedbacks,
     createTherNineFeedback,
     updateTherNineFeedback,
+    deleteTherNineFeedback,
 };
