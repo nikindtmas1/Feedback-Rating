c9bdef2ae9553755f2ddf7302c17745fd0d0a673
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-10 14:12:01 +0200

    Added edit router

--- server/src/controllers/therNineController.js
+++ server/src/controllers/therNineController.js
@@ -12,3 +12,8 @@ router.post('/', async (req, res) => {
     await therNineService.createTherNineFeedback({...req.body});
     res.json({ ok: true });
 });
+
+router.put('/:id', async (req, res) => {
+    await therNineService.updateTherNineFeedback(req.params.id, req.body);
+    res.json({ ok: true });
+});
