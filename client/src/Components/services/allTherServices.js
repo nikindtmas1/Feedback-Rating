0bb0ea094541acd4f9f80d595e9c54acf5c97c08
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-14 10:42:10 +0200

    Create allDataServices.js

---
+++ client/src/Components/services/allTherServices.js
@@ -0,0 +1,10 @@
+import * as goshoDataServices from '../services/goshoData';
+import * as peshoDataServices from '../services/peshoData';
+import * as toshoDataServices from '../services/toshoData';
+
+
+export const allServicesData = {
+    goshoDataServices,
+    peshoDataServices,
+    toshoDataServices,
+};
