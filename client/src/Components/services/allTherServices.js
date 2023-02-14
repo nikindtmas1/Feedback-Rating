8526dc0b3b5a49a9c285a115967a3ab08da6a614
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-02-14 10:50:08 +0200

    Import all therServicesdata

--- client/src/Components/services/allTherServices.js
+++ client/src/Components/services/allTherServices.js
@@ -1,10 +1,24 @@
 import * as goshoDataServices from '../services/goshoData';
 import * as peshoDataServices from '../services/peshoData';
 import * as toshoDataServices from '../services/toshoData';
+import * as tomiDataServices from '../services/tomiData';
+import * as therFiveServices from '../services/therapiestFiveData';
+import * as therSixServices from '../services/therSixData';
+import * as therSevenServices from '../services/therSeventhData';
+import * as therEightServices from '../services/therEightData';
+import * as therNineServices from '../services/therNineData';
+import * as therTenServices from '../services/therTenData';
 
 
 export const allServicesData = {
     goshoDataServices,
     peshoDataServices,
     toshoDataServices,
+    tomiDataServices,
+    therFiveServices,
+    therSixServices,
+    therSevenServices,
+    therEightServices,
+    therNineServices,
+    therTenServices,
 };
